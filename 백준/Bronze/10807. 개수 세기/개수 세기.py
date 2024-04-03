ddefeb3f7f23cce4d8ea54ff36df6cc6164bf6e0
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-04-03 13:43:06 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,14 +1,7 @@
-# 첫째 줄 입력: 정수의 개수 N
-N = int(input())
+n = int(input())
 
-# 둘째 줄 입력: N개의 정수
-integers = list(map(int, input().split()))
+A = list(map(int, input().split()))
 
-# 셋째 줄 입력: 찾으려는 정수 v
 v = int(input())
 
-# 정수 v의 등장 횟수 세기
-count_v = integers.count(v)
-
-# 결과 출력
-print(count_v)
+print(A.count(v))
