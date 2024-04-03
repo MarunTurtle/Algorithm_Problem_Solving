81a819c564986e5fb9b087ea6d4a41859c755213
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-04-03 13:38:36 +0900

    [Bronze V] Title: X보다 작은 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,11 +1,5 @@
-# 첫째 줄 입력 받기: N과 X
-N, X = map(int, input().split())
-
-# 둘째 줄 입력 받기: 수열 A
-A = list(map(int, input().split()))
-
-# X보다 작은 수를 찾아 출력하기
-result = [str(a) for a in A if a < X]
-
-# 결과를 공백으로 구분하여 출력
-print(' '.join(result))
+n, x = map(int, input().split())
+numbers = input().split()
+filtered_numbers = [num for num in numbers if int(num) < x]
+answer = ' '.join(filtered_numbers)
+print(answer)
