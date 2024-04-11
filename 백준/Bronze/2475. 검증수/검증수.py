53df97a6083c14acb2896e3a1abbac8793fa53b0
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-04-11 17:56:04 +0900

    [Bronze V] Title: 검증수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,7 +1,3 @@
-a = list(map(int, input().split()))
+A = list(map(int, input().split()))
 
-def func(x):
-  result = sum(i**2 for i in x)
-  return result % 10
-
-print(func(a))
+print(sum(a**2 for a in A)%10)
