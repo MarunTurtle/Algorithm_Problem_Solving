643863a50eaf24543f8ed99950d15afbe94f15fd
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-04-12 15:12:57 +0900

    [Bronze II] Title: 숫자의 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -5,17 +5,8 @@ C = int(input())
 
 # Product of A, B, C
 product = A * B * C
+P = list(str(product))  # Convert product to string, then to list of its characters
 
-# Array to count digits from 0 to 9
-digit_count = [0] * 10
-
-# Count digits using arithmetic
-temp = product
-while temp > 0:
-    last_digit = temp % 10  # Get the last digit
-    digit_count[last_digit] += 1  # Increment the count for this digit
-    temp //= 10  # Remove the last digit
-
-# Output the count of each digit
-for count in digit_count:
-    print(count)
+# Count numbers of 0-9
+for i in range(10):
+    print(P.count(str(i)))  # Convert i to string before counting
