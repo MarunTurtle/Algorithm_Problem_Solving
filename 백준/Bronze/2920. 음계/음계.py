2feeb215646feb770f86a8ab98fe015a4b828258
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-04-15 20:09:08 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,12 +1,8 @@
-def classify_sequence(sequence):
-    if sequence == [1, 2, 3, 4, 5, 6, 7, 8]:
-        return "ascending"
-    elif sequence == [8, 7, 6, 5, 4, 3, 2, 1]:
-        return "descending"
-    else:
-        return "mixed"
-
-# 예제 입력 처리
 sequence = list(map(int, input().split()))
-result = classify_sequence(sequence)
-print(result)
+
+if sequence == sorted(sequence):
+    print("ascending")
+elif sequence == sorted(sequence, reverse = True):
+    print("descending")
+else:
+    print("mixed")
