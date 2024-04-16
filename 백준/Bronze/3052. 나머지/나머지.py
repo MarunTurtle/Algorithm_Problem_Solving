2e046f89ac9b525ec7925ff945e186bd5e75cba8
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-04-16 17:41:59 +0900

    [Bronze II] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,11 +1,2 @@
-A = []
-for _ in range(10):
-    a = int(input())
-    A.append(a)
-
-B = []
-for a in A:
-    b = a % 42
-    B.append(b)
-
+B = [a % 42 for a in [int(input()) for _ in range(10)]]
 print(len(set(B)))
