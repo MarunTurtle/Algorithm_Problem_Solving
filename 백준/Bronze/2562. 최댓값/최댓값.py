0aa8f8b1b83da78d623faaf1b16d337b31d0215e
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-04-19 20:43:42 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,10 +1,3 @@
-numbers = []
+A = [int(input()) for i in range(9)]
 
-for i in range(9):
-  a = int(input())
-  numbers.append(a)
-
-max_value = max(numbers)
-index_max = numbers.index(max_value) + 1
-
-print(max_value, index_max, sep="\n")
+print(max(A), A.index(max(A))+1, sep="\n")
