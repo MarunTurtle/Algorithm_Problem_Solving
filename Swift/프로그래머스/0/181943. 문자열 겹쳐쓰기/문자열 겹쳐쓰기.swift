744f35ff1a1ac1371156d18ff5df9460ebb12950
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-05-21 09:58:12 +0900

    Update 문자열 겹쳐쓰기.swift
    Added one more solution

--- Swift/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.swift
+++ Swift/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.swift
@@ -5,3 +5,16 @@ func solution(_ my_string:String, _ overwrite_string:String, _ s:Int) -> String 
     let suffix = my_string.dropFirst(s + overwrite_string.count)
     return prefix + overwrite_string + suffix
 }
+
+
+import Foundation
+
+func solution(_ my_string: String, _ overwrite_string: String, _ s: Int) -> String {
+    var result = my_string
+    let startIndex = my_string.index(my_string.startIndex, offsetBy: s)
+    let endIndex = my_string.index(startIndex, offsetBy: overwrite_string.count)
+    
+    result.replaceSubrange(startIndex..<endIndex, with: overwrite_string)
+    
+    return result
+}
