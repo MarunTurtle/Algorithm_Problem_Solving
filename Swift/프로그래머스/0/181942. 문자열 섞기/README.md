84ecaff1b26c6c15793b70af75e234febb27bb5d
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-05-22 11:54:46 +0900

    [level 0] Title: 문자열 섞기, Time: 0.08 ms, Memory: 16 MB -BaekjoonHub

--- Swift/프로그래머스/0/181942. 문자열 섞기/README.md
+++ Swift/프로그래머스/0/181942. 문자열 섞기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 섞기 - 181942 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181942?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181942) 
 
 ### 성능 요약
 
-메모리: 15.9 MB, 시간: 0.01 ms
+메모리: 16 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 22일 11:51:36
+2024년 05월 22일 11:54:43
 
 ### 문제 설명
 
--- Swift/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
+++ Swift/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
@@ -1,20 +1,5 @@
-func solution(_ str1: String, _ str2: String) -> String {
-    var result = ""
-    
-    let length1 = str1.count
-    let length2 = str2.count
-    let maxLength = max(length1, length2)
-    
-    for i in 0..<maxLength {
-        if i < length1 {
-            let index = str1.index(str1.startIndex, offsetBy: i)
-            result.append(str1[index])
-        }
-        if i < length2 {
-            let index = str2.index(str2.startIndex, offsetBy: i)
-            result.append(str2[index])
-        }
-    }
-    
-    return result
+import Foundation
+
+func solution(_ str1:String, _ str2:String) -> String {
+    return zip(str1, str2).map { String($0) + String($1) }.joined()
 }
