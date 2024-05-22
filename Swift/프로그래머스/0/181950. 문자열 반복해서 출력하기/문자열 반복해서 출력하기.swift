31d144c75ba4cf1922e754c8714e4e15116cff21
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-05-22 11:22:52 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 5.73 ms, Memory: 13.3 MB -BaekjoonHub

--- Swift/프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.swift
+++ Swift/프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.swift
@@ -2,6 +2,4 @@ import Foundation
 
 let inp = readLine()!.split(separator: " ")
 let (str, n) = (String(inp[0]), Int(inp[1])!)
-for _ in 0..<n {
-    print(str, terminator: "")
-}
+print(String(repeating: str, count: n))
