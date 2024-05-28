18226ce8b00839cfa4a606dd09f5f904d504e3f0
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-05-28 10:32:43 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 5.41 ms, Memory: 12.8 MB -BaekjoonHub

--- Swift/프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.swift
+++ Swift/프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.swift
@@ -1,7 +1,13 @@
 import Foundation
 
-let inp = String(readLine()!)
-
-for char in inp {
-    print(char.isLowercase ? char.uppercased() : char.isUppercase ? char.lowercased() : String(char), terminator: "")
+// Read a line from standard input
+if let input = readLine() {
+    // Convert each character: lowercase to uppercase, and uppercase to lowercase
+    let transformed = input.map { character in
+        // Check if the character is lowercase, convert to uppercase if true, otherwise convert to lowercase
+        character.isLowercase ? character.uppercased() : character.lowercased()
+    }.joined()
+    
+    // Print the transformed string
+    print(transformed)
 }
