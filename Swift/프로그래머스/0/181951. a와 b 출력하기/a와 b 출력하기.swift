f6e1d378cdc917db39a38f16d4e2a9cebe7e1d32
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-05-28 10:18:01 +0900

    [level 0] Title: a와 b 출력하기, Time: 7.50 ms, Memory: 13 MB -BaekjoonHub

--- Swift/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.swift
+++ Swift/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-let n = readLine()!.split(separator: " ").map { Int($0)! }
-let (a, b) = (n[0], n[1])
-print("a = \(a)\nb = \(b)")
+let inp = readLine()!.split(separator: " ")
+print("a = \(inp[0])")
+print("b = \(inp[1])")
