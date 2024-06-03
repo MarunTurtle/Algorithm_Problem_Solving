b24b2290a670a74f98b7920580b52b3618764636
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-06-03 09:31:05 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ Python/백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,13 +1,9 @@
 n, m = map(int, input().split())
 
-baskets = [i for i in range(1, n+1)]
+baskets = list(range(1, n + 1))
 
 for _ in range(m):
     i, j = map(int, input().split())
-    while i < j:
-        baskets[i-1], baskets[j-1] = baskets[j-1], baskets[i-1]
-        i += 1
-        j -= 1
+    baskets[i-1:j] = baskets[i-1:j][::-1]
 
 print(' '.join(map(str, baskets)))
-
