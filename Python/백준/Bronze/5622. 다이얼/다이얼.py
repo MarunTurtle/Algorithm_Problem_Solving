1f66603755c8d7c7bf0195d918b06c7cc69cc81c
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-06-08 09:10:32 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/5622. 다이얼/다이얼.py
+++ Python/백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,28 +1,12 @@
-def a_to_i(text):
-    result = 0
-    for a in text:
-        int_a = ord(a)
-        if 65 <= int_a <= 67:        # 'A', 'B', 'C'
-            result += 3
-        elif 68 <= int_a <= 70:      # 'D', 'E', 'F'
-            result += 4
-        elif 71 <= int_a <= 73:      # 'G', 'H', 'I'
-            result += 5
-        elif 74 <= int_a <= 76:      # 'J', 'K', 'L'
-            result += 6
-        elif 77 <= int_a <= 79:      # 'M', 'N', 'O'
-            result += 7
-        elif 80 <= int_a <= 83:      # 'P', 'Q', 'R', 'S'
-            result += 8
-        elif 84 <= int_a <= 86:      # 'T', 'U', 'V'
-            result += 9
-        elif 87 <= int_a <= 90:      # 'W', 'X', 'Y', 'Z'
-            result += 10
-    return result
+def dial_time(word):
+    dial_str = 'ABC DEF GHI JKL MNO PQRS TUV WXYZ'
+    dial = {char: idx + 3 for idx, group in enumerate(dial_str.split()) for char in group}
+    
+    return sum(dial[char] for char in word)
 
 def main():
-    text = str(input())
-    print(a_to_i(text))
+    text = input().strip()
+    print(dial_time(text))
 
 if __name__ == "__main__":
     main()
