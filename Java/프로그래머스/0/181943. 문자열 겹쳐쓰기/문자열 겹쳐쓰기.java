32d3c5c06002c4c912b5446e62e51a4a19487a11
Jaehyun Kevin Lee <angle0918@naver.com>
2024-07-29 16:47:21 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.09 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static String solution(String my_string, String overwrite_string, int s) {
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        sb.replace(s, s+overwrite_string.length(), overwrite_string);
+        
+        return sb.toString();
+    }
+}
