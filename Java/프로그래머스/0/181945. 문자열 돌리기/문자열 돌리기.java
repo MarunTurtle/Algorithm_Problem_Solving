ed954f0544258c56644b32a8f468db3975367a39
Jaehyun Kevin Lee <angle0918@naver.com>
2024-07-29 16:23:08 +0900

    [level 0] Title: 문자열 돌리기, Time: 191.05 ms, Memory: 66 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        for (char ch : word.toCharArray())
+            System.out.println(ch);
+        }       
+}
