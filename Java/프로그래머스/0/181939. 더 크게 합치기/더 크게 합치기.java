e07b988874b9e008ff9b5533215bc780e0ab6f56
Jaehyun Kevin Lee <angle0918@naver.com>
2024-07-30 22:55:37 +0900

    [level 0] Title: 더 크게 합치기, Time: 22.11 ms, Memory: 68.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,32 +1,46 @@
 class Solution {
     public int solution(int a, int b) {
         
-        // Declare two String variables by type casting
-        String aString = Integer.toString(a);
-        String bString = Integer.toString(b);
+        // Best Solution
+        int answer = 0;
         
-        // First addition
-        String sum1 = aString + bString;
+        // Use String concatenation ""
+        int aLong = Integer.parseInt("" + a + b);
+        int bLong = Integer.parseInt("" + b + a);
         
-        // Second addition
-        String sum2 = bString + aString;
+        // Ternary Operator
+        answer = aLong > bLong ? aLong : bLong;
+
+        return answer;
         
-        // Re-declare two int variables by type casting
-        int intsum1 = Integer.valueOf(sum1);
-        int intsum2 = Integer.valueOf(sum2);   
-        
-        // Declare variable to store the result
-        int res;
-        
-        // Compare the two ints
-        if (intsum1 > intsum2) {
-            res = intsum1;
-        } else {
-            res = intsum2;
-        }
-        
-        // Return the greater number
-        return res;
+        // My initial solution
+//         // Declare variable to store the result
+//         int res;
+//         
+//         // Declare two String variables by type casting
+//         String aString = Integer.toString(a);
+//         String bString = Integer.toString(b);
+//         
+//         // First addition
+//         String sum1 = aString + bString;
+//         
+//         // Second addition
+//         String sum2 = bString + aString;
+//         
+//         // Re-declare two int variables by type casting
+//         int intsum1 = Integer.valueOf(sum1);
+//         int intsum2 = Integer.valueOf(sum2);   
+//         
+//         
+//         // Compare the two ints
+//         if (intsum1 > intsum2) {
+//             res = intsum1;
+//         } else {
+//             res = intsum2;
+//         }
+//         
+//         // Return the greater number
+//         return res;
         
     }
 }
