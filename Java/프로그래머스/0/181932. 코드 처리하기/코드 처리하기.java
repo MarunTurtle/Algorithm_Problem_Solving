6ff74b00879e5f4cf4d94a4610c14f1d8f1d1882
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-05 22:27:49 +0900

    [level 0] Title: 코드 처리하기, Time: 10.62 ms, Memory: 76.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ Java/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,3 +1,22 @@
+// class Solution {
+//     public String solution(String code) {
+//         StringBuilder answer = new StringBuilder();
+//         int mode = 0;
+//         for (int i = 0; i < code.length(); i++) {
+//             char current = code.charAt(i);
+//             if (current == '1') {
+//                 mode = mode == 0 ? 1 : 0;
+//                 continue;
+//             }
+
+//             if (i % 2 == mode) {
+//                 answer.append(current);
+//             }
+//         }
+//         return answer.length() == 0 ? "EMPTY" : answer.toString();
+//     }
+// }
+
 class Solution {
     public String solution(String code) {
         // Declare variables
