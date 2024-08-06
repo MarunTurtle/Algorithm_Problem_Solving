7e4fb1da414d87f40404101ff5fde04f057376bd
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-06 15:37:03 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.30 ms, Memory: 77.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ Java/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -15,3 +15,13 @@ class Solution {
         return product < Math.pow(sum, 2) ? 1 : 0;
     }
 }
+
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[] num_list) {
+//         int sum = Arrays.stream(num_list).reduce(Integer::sum).getAsInt();
+//         int product = Arrays.stream(num_list).reduce((i,j)->i*j).getAsInt();
+//         return product < sum*sum ? 1 : 0;
+//     }
+// }
