4b6d2b6ecca4086b92c83c05d8190d3939c03183
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-06 15:45:38 +0900

    [level 0] Title: 이어 붙인 수, Time: 25.78 ms, Memory: 83.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ Java/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -16,3 +16,26 @@ class Solution {
 
     }
 }
+
+
+// class Solution {
+//     public int solution(int[] num_list) {
+//         int answer = 0;
+
+//         int even = 0;
+//         int odd = 0;
+
+//         for(int num : num_list) {
+//             if(num % 2 == 0) {
+//                 even *= 10;
+//                 even += num;
+//             } else {
+//                 odd *= 10;
+//                 odd += num;
+//             }
+//         }
+//         answer = even + odd;
+
+//         return answer;
+//     }
+// }
