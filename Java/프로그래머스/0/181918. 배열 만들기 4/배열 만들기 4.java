9e8f488f180257fcf30c8e621b5fb5f595bd23c5
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-07 16:34:32 +0900

    [level 0] Title: 배열 만들기 4, Time: 24.77 ms, Memory: 91.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ Java/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,16 +1,25 @@
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 class Solution {
     public int[] solution(int[] arr) {
 
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
 
         for (int num : arr) {
-            while (!stack.isEmpty() && num <= stack.peek())
+            // Ensure stack contains only elements in increasing order
+            while (!stack.isEmpty() && num <= stack.peek()) {
                 stack.pop();
+            }
             stack.push(num);
         }
 
-        return stack.stream().mapToInt(i -> i).toArray();
+        // Convert Deque to int[]
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
     }
 }
