fdee0c697383ed5e62db2804debeb4c9d9141dc2
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-08 13:51:50 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 4.18 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(String number) {
+        
+        int sum = 0;
+        int length = number.length();
+
+        for (int i = 0; i < length; i++) {
+            sum += number.charAt(i) - '0';
+        }
+
+        int mod = sum % 9;
+        return mod;
+    } 
+}
+
+// class Solution {
+//     public int solution(String number) {
+//         int sum = 0;
+
+//         for (String numStr : number.split("")) {
+//             int digit = Integer.parseInt(numStr);
+//             sum += digit;
+//         }
+
+//         int result = sum % 9;
+//         return result;
+//     }
+// }
