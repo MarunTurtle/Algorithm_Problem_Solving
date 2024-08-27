e865da90625d3edaba5f5e9fded3355e805eb336
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-27 09:19:46 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.13 ms, Memory: 73.5 MB -BaekjoonHub

--- Java/프로그래머스/0/181910. 문자열의 뒤의 n글자/README.md
+++ Java/프로그래머스/0/181910. 문자열의 뒤의 n글자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열의 뒤의 n글자 - 181910 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181910?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181910) 
 
 ### 성능 요약
 
-메모리: 72 MB, 시간: 0.09 ms
+메모리: 73.5 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 27일 09:18:06
+2024년 08월 27일 09:19:45
 
 ### 문제 설명
 
--- Java/프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
+++ Java/프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -1,11 +1,15 @@
 class Solution {
     public String solution(String my_string, int n) {
+        // Declare answer
         StringBuilder answer = new StringBuilder();
         
+        // Declare length of the word
         int length = my_string.length();
         
+        // Add the substring to the answer
         answer.append(my_string.substring(length - n, length)); 
-                      
+        
+        // Return the answer
         return answer.toString();
     }
 }
