00568f894642b46120ca05cfce5b02619aa5d01e
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-27 09:42:58 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.07 ms, Memory: 69.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ Java/프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -14,3 +14,9 @@ class Solution {
         return answer; 
     }
 }
+
+// class Solution {
+//     public int solution(String myString, String isSuffix) {
+//         return myString.endsWith(isSuffix) ? 1 : 0;
+//     }
+// }
