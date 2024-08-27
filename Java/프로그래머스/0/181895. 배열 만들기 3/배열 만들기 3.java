061ce00c4f898b70b8229fdd2ccf6fb5eef4ff85
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-27 16:49:35 +0900

    [level 0] Title: 배열 만들기 3, Time: 2.26 ms, Memory: 103 MB -BaekjoonHub

--- Java/프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ Java/프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,17 +1,39 @@
-import java.util.ArrayList;
-import java.util.List;
 
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
+        // 두 구간의 길이를 합산하여 최종 배열의 크기를 계산
+        int size = (intervals[0][1] - intervals[0][0] + 1) + (intervals[1][1] - intervals[1][0] + 1);
+        int[] answer = new int[size];
         
-        List<Integer> answer = new ArrayList<>();
-        
-        for (int i = 0; i < 2; i++) {
-            for (int j = intervals[i][0]; j <= intervals[i][1]; j++) {
-                answer.add(arr[j]);
-            }
+        int index = 0;
+        // 첫 번째 구간
+        for (int j = intervals[0][0]; j <= intervals[0][1]; j++) {
+            answer[index++] = arr[j];
+        }
+        // 두 번째 구간
+        for (int j = intervals[1][0]; j <= intervals[1][1]; j++) {
+            answer[index++] = arr[j];
         }
         
-        return answer.stream().mapToInt(i -> i).toArray();
+        return answer;
     }
 }
+
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// class Solution {
+//     public int[] solution(int[] arr, int[][] intervals) {
+        
+//         List<Integer> answer = new ArrayList<>();
+        
+//         for (int i = 0; i < 2; i++) {
+//             for (int j = intervals[i][0]; j <= intervals[i][1]; j++) {
+//                 answer.add(arr[j]);
+//             }
+//         }
+        
+//         return answer.stream().mapToInt(i -> i).toArray();
+//     }
+// }
