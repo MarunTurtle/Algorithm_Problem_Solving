ec79540db3095c9c14e2ccc56e23458317d732d0
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-27 10:49:31 +0900

    [level 0] Title: 세로 읽기, Time: 0.22 ms, Memory: 73.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ Java/프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -13,3 +13,16 @@ class Solution {
         return answer.toString();
     }
 }
+
+
+// class Solution {
+
+//     public String solution(String my_string, int m, int c) {
+//         String answer = "";
+
+//         for (int i = c - 1; i < my_string.length(); i += m) {
+//             answer += my_string.charAt(i);
+//         }
+//         return answer;
+//     }
+// }
