c8e80a14b9cffd26d0577802eba8ec669d401df1
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-27 21:12:43 +0900

    Update n개 간격의 원소들.java

--- Java/프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ Java/프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -14,3 +14,23 @@ class Solution {
         return resultList.stream().mapToInt(Integer::intValue).toArray();
     }
 }
+
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        // Calculate the length of the resulting array
+        int length = (num_list.length + n - 1) / n;
+
+        // Create the result array with the calculated length
+        int[] result = new int[length];
+        
+        // Fill the result array by picking every n-th element
+        for (int i = 0, j = 0; i < num_list.length; i += n, j++) {
+            result[j] = num_list[i];
+        }
+        
+        return result;
+    }
+}
