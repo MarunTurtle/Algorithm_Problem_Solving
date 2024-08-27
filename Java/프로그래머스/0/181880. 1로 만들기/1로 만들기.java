cb3b935263f5d61c9b801824c268e85068bb43df
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-28 08:12:56 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ Java/프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int[] num_list) {
-        
         // Declare count
         int count = 0;
         
@@ -9,20 +8,23 @@ class Solution {
         
         // While loop until every number becomes one
         while (true) {
-            // Declare boolean changed
-            Boolean changed = false;
+            boolean changed = false;  // Reset changed to false at the start of each iteration
             
             for (int i = 0; i < length; i++) {
-                if (num_list[i] != 1) {
-                    if (num_list[i] % 2 == 0) num_list[i] /= 2; 
-                    else num_list[i] = (num_list[i] - 1) / 2;
-                    changed = true;
-                    count++;
+                if (num_list[i] != 1) {  // If the number is not 1
+                    if (num_list[i] % 2 == 0) {
+                        num_list[i] /= 2;  // If even, divide by 2
+                    } else {
+                        num_list[i] = (num_list[i] - 1) / 2;  // If odd, subtract 1 and then divide by 2
+                    }
+                    changed = true;  // A change occurred
+                    count++;  // Increment count for each operation performed
                 }
             }
-            if (!changed) break;
+            
+            if (!changed) break;  // Exit the loop if no changes occurred
         }
         
-        return count;
+        return count;  // Return the total count of operations
     }
 }
