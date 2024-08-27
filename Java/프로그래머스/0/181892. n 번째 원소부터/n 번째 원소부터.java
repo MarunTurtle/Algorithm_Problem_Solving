56184e27bf0cb022ef7b998a1ed9ea5baf62037f
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-27 17:50:34 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ Java/프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {   
         
-        
         int length = num_list.length;
         
-        int[] answer = new int[length + 1 - n];
-       
-            
-        System.arraycopy(num_list, n-1, answer, 0, length + 1 - n);
-    
-            
-        return answer;
+        int[] answers = new int[length + 1 - n];
+           
+        System.arraycopy(num_list, n-1, answers, 0, length + 1 - n);
+         
+        return answers;
     }
 }
