5a44551bb45596e620c55fbbbfc0212df82fda40
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-28 09:56:02 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.26 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        
+        int count = 0;
+        
+        for (int i = 0; i < myString.length() - pat.length() + 1; i++) {
+            String subString = myString.substring(i, i + pat.length());
+            if (subString.startsWith(pat)) count++;    
+        }
+        
+        return count;
+    }
+}
