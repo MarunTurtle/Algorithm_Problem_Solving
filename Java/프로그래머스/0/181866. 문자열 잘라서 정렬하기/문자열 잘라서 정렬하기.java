6f9b09d909a9efa8a39aa15b8fe5765cd62e34ea
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-28 10:51:57 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 25.98 ms, Memory: 98.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ Java/프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -9,12 +9,9 @@ class Solution {
         
         // Filter out empty strings, sort the remaining strings lexicographically,
         // and collect them into a list
-        List<String> filteredAndSorted = Arrays.stream(parts)
-                                               .filter(s -> !s.isEmpty())  // Remove empty strings
-                                               .sorted()                   // Sort lexicographically
-                                               .collect(Collectors.toList());
-        
-        // Convert the list back to an array and return it
-        return filteredAndSorted.toArray(new String[0]);
+        return Arrays.stream(parts)
+                     .filter(s -> !s.isEmpty())  // Remove empty strings
+                     .sorted()                   // Sort lexicographically
+                     .toArray(String[]::new);
     }
 }
