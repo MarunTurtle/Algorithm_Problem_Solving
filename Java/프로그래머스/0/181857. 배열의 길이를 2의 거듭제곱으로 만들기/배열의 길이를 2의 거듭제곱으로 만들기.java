5d9a71c0b47b96348ca0cdd248cf689a835c74ff
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-29 15:57:54 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.06 ms, Memory: 78.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
+++ Java/프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
@@ -1,29 +1,43 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
+
+// class Solution {
+//     public int[] solution(int[] arr) {
+//         int length = arr.length;
+        
+//         // Check if the length is already a power of 2
+//         int power = 1;
+//         while (power < length) {
+//             power *= 2;
+//         }
+        
+//         // If length is already a power of 2, return arr as is
+//         if (power == length) {
+//             return arr;
+//         }
+        
+//         // Otherwise, create a new array of size `power` and copy arr into it
+//         int[] result = new int[power];
+        
+//         for (int i = 0; i < length; i++) {
+//             result[i] = arr[i];
+//         }
+        
+//         // The remaining elements of the array are initialized to 0 by default in Java
+        
+//         return result;
+//     }
+// }
+
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        int length = arr.length;
-        
-        // Check if the length is already a power of 2
-        int power = 1;
-        while (power < length) {
-            power *= 2;
+        int length = 1;
+
+        while (length < arr.length) {
+            length *= 2;
         }
-        
-        // If length is already a power of 2, return arr as is
-        if (power == length) {
-            return arr;
-        }
-        
-        // Otherwise, create a new array of size `power` and copy arr into it
-        int[] result = new int[power];
-        
-        for (int i = 0; i < length; i++) {
-            result[i] = arr[i];
-        }
-        
-        // The remaining elements of the array are initialized to 0 by default in Java
-        
-        return result;
+
+        return Arrays.copyOf(arr, length);
     }
 }
