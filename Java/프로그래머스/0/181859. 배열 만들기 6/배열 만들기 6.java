933ba8a8c6a8195ef3e7e68535202ad481121f23
Jaehyun Kevin Lee <angle0918@naver.com>
2024-08-29 15:21:42 +0900

    [level 0] Title: 배열 만들기 6, Time: 32.76 ms, Memory: 120 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        // Declare stk as a Deque (double-ended queue)
+        Deque<Integer> stk = new LinkedList<>();        
+        
+        // Iterate through elements in arr
+        for (int i = 0; i < arr.length; i++) {
+            // If the stack is empty, push arr[i]
+            if (stk.isEmpty()) {
+                stk.push(arr[i]);
+            // If the top of the stack is the same as arr[i], pop the stack
+            } else if (stk.peek() == arr[i]) {
+                stk.pop();
+            // If the top of the stack is different, push arr[i]
+            } else {
+                stk.push(arr[i]);
+            }
+        }
+        
+        // If the stack is empty after processing, add -1
+        if (stk.isEmpty()) {
+            stk.push(-1);
+        }
+        
+        // Convert the stack to an array to return
+        int[] result = new int[stk.size()];
+        int i = stk.size() - 1;
+        while (!stk.isEmpty()) {
+            result[i--] = stk.pop();
+        }
+        
+        return result;
+    }
+}
