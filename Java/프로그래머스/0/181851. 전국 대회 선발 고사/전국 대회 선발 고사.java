b2912f5150a4a497276fc788ce37e1d98cbcbe9d
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-02 14:37:04 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.98 ms, Memory: 79.5 MB -BaekjoonHub

--- Java/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ Java/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,25 +1,41 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[] rank, boolean[] attendance) {
+//         // Create a list of students who can attend
+//         List<Integer> attendees = new ArrayList<>();
+        
+//         for (int i = 0; i < rank.length; i++) {
+//             if (attendance[i]) {
+//                 attendees.add(i);
+//             }
+//         }
+        
+//         // Sort the attendees based on their ranks
+//         attendees.sort(Comparator.comparingInt(i -> rank[i]));
+        
+//         // Get the top 3 students
+//         int a = attendees.get(0);
+//         int b = attendees.get(1);
+//         int c = attendees.get(2);
+        
+//         // Return the result based on the formula
+//         return 10000 * a + 100 * b + c;
+//     }
+// }
+
+
+import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        // Create a list of students who can attend
-        List<Integer> attendees = new ArrayList<>();
-        
-        for (int i = 0; i < rank.length; i++) {
-            if (attendance[i]) {
-                attendees.add(i);
-            }
+
+        PriorityQueue<Integer> que = new PriorityQueue<>((a, b) -> rank[a] - rank[b]);
+        for (int i = 0; i < attendance.length; i++) {
+            if (attendance[i])
+                que.add(i);
         }
-        
-        // Sort the attendees based on their ranks
-        attendees.sort(Comparator.comparingInt(i -> rank[i]));
-        
-        // Get the top 3 students
-        int a = attendees.get(0);
-        int b = attendees.get(1);
-        int c = attendees.get(2);
-        
-        // Return the result based on the formula
-        return 10000 * a + 100 * b + c;
+
+        return que.poll() * 10000 + que.poll() * 100 + que.poll();
     }
 }
