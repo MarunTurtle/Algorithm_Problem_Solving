c50ec25e64ec510e59083b423c17c048324ac731
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-02 14:47:50 +0900

    [level 0] Title: 0 떼기, Time: 7.78 ms, Memory: 84.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ Java/프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,9 +1,15 @@
 class Solution {
     public String solution(String n_str) {
-        // Convert the string to an integer (this removes any leading zeros)
-        int num = Integer.parseInt(n_str);
-        
-        // Convert the integer back to a string
-        return String.valueOf(num);
+        return ""+Integer.parseInt(n_str);
     }
 }
+
+// class Solution {
+//     public String solution(String n_str) {
+//         // Convert the string to an integer (this removes any leading zeros)
+//         int num = Integer.parseInt(n_str);
+        
+//         // Convert the integer back to a string
+//         return String.valueOf(num);
+//     }
+// }
