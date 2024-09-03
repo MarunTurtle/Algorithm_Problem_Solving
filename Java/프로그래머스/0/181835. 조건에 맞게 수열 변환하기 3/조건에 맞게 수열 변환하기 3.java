c5b636fcaa6dd1baf098d0318544e556d38b699b
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-03 15:01:55 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 10.79 ms, Memory: 167 MB -BaekjoonHub

--- Java/프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
+++ Java/프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -1,12 +1,32 @@
+// class Solution {
+//     public int[] solution(int[] arr, int k) {
+//         if (k % 2 == 0) {
+//             for (int i = 0; i < arr.length; i++) {
+//                 arr[i] += k;
+//             }
+//         } else {
+//             for (int i = 0; i < arr.length; i++) {
+//                 arr[i] *= k;
+//             }
+//         }
+//         return arr;
+//     }
+// }
+
+
 class Solution {
     public int[] solution(int[] arr, int k) {
+        int index = 0;  // Auxiliary index variable
         if (k % 2 == 0) {
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] += k;
+            for (int n : arr) {
+                arr[index] = n + k;
+                index++;
             }
         } else {
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] *= k;
+            index = 0;  // Resetting the index for the second loop
+            for (int n : arr) {
+                arr[index] = n * k;
+                index++;
             }
         }
         return arr;
