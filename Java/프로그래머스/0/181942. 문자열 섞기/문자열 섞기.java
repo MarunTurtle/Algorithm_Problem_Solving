97e2c09489d34779e998171b7d82e7269dc1fc84
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-05 08:36:44 +0900

    [level 0] Title: 문자열 섞기, Time: 0.07 ms, Memory: 73.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,29 +1,15 @@
-class Solution {
-    public String solution(String str1, String str2) {
+public class Solution {
+    public String solution (String str1, String str2) {
         
-//         //Declare StringBuilder result, str1.length(), str2.length() 
-//         StringBuilder result = new StringBuilder();
-//         int n = str1.length();
-//         int m = str2.length();
+        StringBuilder result = new StringBuilder();
         
-        
-//         //Utilize for loop to iterate each strings and append to the result 
-//         for (int i = 0; i < n; i++) {
-//             result.append(str1.charAt(i));
-//             result.append(str2.charAt(i));       
-//         }
-        
-//         //Print result
-//         return result.toString();
-//     }
-        String answer = "";
-
-        for(int i = 0; i < str1.length(); i++){
-            answer+= str1.charAt(i);
-            answer+= str2.charAt(i);
+        int len = str1.length();
+        for (int i = 0; i < len; i++) {
+            result.append(str1.charAt(i));     
+            result.append(str2.charAt(i));
         }
-
-        return answer;
+        
+        return result.toString();
+        
     }
-
 }
