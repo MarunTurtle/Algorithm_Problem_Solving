1a0517808b22a9bb2ed5a8e3b3b8232999619adf
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-05 08:51:43 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.04 ms, Memory: 70.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ Java/프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,19 +1,7 @@
-class Solution {
-    public String solution(String my_string, int k) {
+public class Solution {
+    public String solution (String my_string, int k) {
         
-        // Best Solution
         return my_string.repeat(k);
         
-        // My Initial Soluiton
-//         // Declare StringBuilder sb
-//         StringBuilder sb = new StringBuilder();
-//         
-//         // Append my_string k times 
-//         for (int i = 0; i < k; i++) {
-//             sb.append(my_string);
-//         }
-//         
-//         // Return the result by using toString() method
-//         return sb.toString();
     }
 }
