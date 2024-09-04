2f13ab5bf9f98eb481598da24a388ac0cb3794b2
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-04 14:30:50 +0900

    [level 0] Title: a와 b 출력하기, Time: 251.70 ms, Memory: 74.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
+++ Java/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -1,12 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println("a = "+a);
-        System.out.println("b = "+b);
-
-        }
+    public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("a = " + sc.nextInt() + "\nb = " + sc.nextInt());
+        
+    }
 }
