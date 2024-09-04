d200dd47a220c43e921baa9272c639f651626741
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-04 14:53:47 +0900

    [level 0] Title: 특수문자 출력하기, Time: 121.88 ms, Memory: 65.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ Java/프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -1,7 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+
         System.out.print("!@#$%^&*(\\'\"<>?:;");
+
     }
 }
