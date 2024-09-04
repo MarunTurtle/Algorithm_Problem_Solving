8fc773a3e63b469ccf492f6755072872fcbe51f3
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-04 16:41:38 +0900

    [level 0] Title: 더 크게 합치기, Time: 10.64 ms, Memory: 82.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,46 +1,12 @@
-class Solution {
-    public int solution(int a, int b) {
-        
-        // Best Solution
-        int answer = 0;
-        
-        // Use String concatenation ""
-        int aLong = Integer.parseInt("" + a + b);
-        int bLong = Integer.parseInt("" + b + a);
-        
-        // Ternary Operator
-        answer = aLong > bLong ? aLong : bLong;
+import java.util.*;
 
-        return answer;
+class Solution {
+    public int solution (int a, int b) {
         
-        // My initial solution
-//         // Declare variable to store the result
-//         int res;
-//         
-//         // Declare two String variables by type casting
-//         String aString = Integer.toString(a);
-//         String bString = Integer.toString(b);
-//         
-//         // First addition
-//         String sum1 = aString + bString;
-//         
-//         // Second addition
-//         String sum2 = bString + aString;
-//         
-//         // Re-declare two int variables by type casting
-//         int intsum1 = Integer.valueOf(sum1);
-//         int intsum2 = Integer.valueOf(sum2);   
-//         
-//         
-//         // Compare the two ints
-//         if (intsum1 > intsum2) {
-//             res = intsum1;
-//         } else {
-//             res = intsum2;
-//         }
-//         
-//         // Return the greater number
-//         return res;
+        int ab = Integer.parseInt("" + a + b);
+        int ba = Integer.parseInt("" + b + a);
+        
+        return Math.max(ab, ba);
         
     }
 }
