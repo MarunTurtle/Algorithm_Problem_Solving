5e01f6420e6fe95a653320f944015ac9a57b5427
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-05 12:19:41 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 81.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ Java/프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,14 +1,33 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
-        // Switch operator
-        switch (ineq) {
-            case "<":
-                return eq.equals("!") ? (n < m ? 1 : 0) : (n <= m ? 1 : 0);
-            case ">":
-                return eq.equals("!") ? (n > m ? 1 : 0) : (n >= m ? 1 : 0);
-            default:
-                // Handle invalid ineq input
-                throw new IllegalArgumentException();
+        
+        if (ineq.equals(">") && eq.equals("!")) {
+            return (n > m) ? 1 : 0;
+        }
+        else if (ineq.equals("<") && eq.equals("!")) {
+            return (n < m) ? 1 : 0;
+        }
+        else if (ineq.equals(">") && eq.equals("=")) {
+            return (n >= m) ? 1 : 0;
+        }
+        else {
+            return (n <= m) ? 1 : 0;
         }
     }
 }
+
+
+// class Solution {
+//     public int solution(String ineq, String eq, int n, int m) {
+//         // Switch operator
+//         switch (ineq) {
+//             case "<":
+//                 return eq.equals("!") ? (n < m ? 1 : 0) : (n <= m ? 1 : 0);
+//             case ">":
+//                 return eq.equals("!") ? (n > m ? 1 : 0) : (n >= m ? 1 : 0);
+//             default:
+//                 // Handle invalid ineq input
+//                 throw new IllegalArgumentException();
+//         }
+//     }
+// }
