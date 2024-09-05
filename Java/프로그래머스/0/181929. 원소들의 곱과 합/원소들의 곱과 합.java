7006bb4f3a1f2b7cc4a6aa6cdad58e0f30d4a0b2
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-05 22:10:43 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.09 ms, Memory: 76.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ Java/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,20 +1,20 @@
-// class Solution {
-//     public int solution(int[] num_list) {
+class Solution {
+    public int solution(int[] num_list) {
         
-//         // Initialize product and sum
-//         int product = 1;
-//         int sum = 0;
+        // Initialize product and sum
+        int product = 1;
+        int sum = 0;
 
-//         // multiply and add all numbers
-//         for (int n : num_list) {
-//             product *= n;
-//             sum += n;
-//         }
+        // multiply and add all numbers
+        for (int n : num_list) {
+            product *= n;
+            sum += n;
+        }
 
-//         // compare the product and sum^2
-//         return product < Math.pow(sum, 2) ? 1 : 0;
-//     }
-// }
+        // compare the product and sum^2
+        return product < Math.pow(sum, 2) ? 1 : 0;
+    }
+}
 
 // import java.util.*;
 
@@ -25,20 +25,3 @@
 //         return product < sum*sum ? 1 : 0;
 //     }
 // }
-
-import java.util.*;
-
-class Solution {
-    public int solution (int[] num_list) {
-        
-        int product = 1; 
-        int sum = 0;
-        
-        for (int i = 0; i < num_list.length; i++) {
-            product *= num_list[i];
-            sum += num_list[i];
-        }
-        
-        return product > (int) Math.pow(sum, 2) ? 0 : 1;
-    }
-}
