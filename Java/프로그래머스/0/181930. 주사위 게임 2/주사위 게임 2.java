d6a62c8470c82e7454693d116d3be0515dd65360
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-05 22:00:29 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.12 ms, Memory: 72 MB -BaekjoonHub

--- Java/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ Java/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -22,3 +22,18 @@ class Solution {
         return answer;
     }
 }
+
+
+// public class Solution {
+//     public int solution(int a, int b, int c) {
+//         if (a != b && b != c && c != a) {
+//             return a + b + c;
+//         } 
+//         else if (a == b && b == c) {
+//             return (int) ((a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3)));
+//         } 
+//         else {
+//             return (int) ((a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)));
+//         }
+//     }
+// }
