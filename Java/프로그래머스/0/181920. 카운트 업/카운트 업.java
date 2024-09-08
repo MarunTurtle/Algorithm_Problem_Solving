6d1b5d9585481acb4423b6945d6a468fb52e8783
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-08 17:26:40 +0900

    [level 0] Title: 카운트 업, Time: 7.91 ms, Memory: 79.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ Java/프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,12 +1,27 @@
-class Solution {
-    public int[] solution(int start_num, int end_num) {
+// class Solution {
+//     public int[] solution(int start_num, int end_num) {
         
-        int[] nums = new int[end_num-start_num+1];
+//         int[] nums = new int[end_num-start_num+1];
 
-        for (int i = start_num; i <= end_num; i++) {
-            nums[i - start_num] = i;            
+//         for (int i = start_num; i <= end_num; i++) {
+//             nums[i - start_num] = i;            
+//         }
+
+//         return nums;
+//     }
+// }
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int s, int e) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+            
+        for (int i = s; i <= e; i++) {
+            list.add(i);
         }
-
-        return nums;
+        
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
