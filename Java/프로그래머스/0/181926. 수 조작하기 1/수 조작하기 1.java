56e27f2990da2d3e09ae5f03f133e5b8cf842067
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-08 15:41:50 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.30 ms, Memory: 85.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ Java/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,19 +1,51 @@
-class Solution {
-    public int solution(int n, String control) {
+// class Solution {
+//     public int solution(int n, String control) {
 
-        // Chnage String to ArrayList
-        // Check every character in the ArrayList
-        for (char cursor : control.toCharArray()) {
-            switch (cursor) {
-                case 'w' -> n += 1;
-                case 'a' -> n -= 10;
-                case 's' -> n -= 1;
-                case 'd' -> n += 10;
-                default -> {
-                }
+//         // Chnage String to ArrayList
+//         // Check every character in the ArrayList
+//         for (char cursor : control.toCharArray()) {
+//             switch (cursor) {
+//                 case 'w' -> n += 1;
+//                 case 'a' -> n -= 10;
+//                 case 's' -> n -= 1;
+//                 case 'd' -> n += 10;
+//                 default -> {
+//                 }
+//             }
+//         }
+
+//         return n;
+//     }
+// }
+
+
+public class Solution {
+    public int solution (int n, String control) {
+        
+        for (char c : control.toCharArray()) {
+            switch (c) {
+                case 'w': 
+                    n += 1;
+                    break;
+                case 'a':
+                    n -= 10;
+                    break;
+                case 's': 
+                    n -= 1;
+                    break;
+                case 'd': 
+                    n += 10;
+                    break;
+                default:
+                    continue;
             }
         }
-
+        
         return n;
     }
 }
+
+
+
+
+
