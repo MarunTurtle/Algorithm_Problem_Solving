f92e688177a47a476ec071379b177cd401da6ee1
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-08 16:00:40 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.08 ms, Memory: 80.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ Java/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,13 +1,33 @@
+// class Solution {
+//     public int[] solution(int[] arr, int[][] queries) {
+//         // Declare temporary int value for swapping
+//         int temp;
+//         for (int[] xy : queries) {
+//             // swap the selected values
+//             temp = arr[xy[0]];
+//             arr[xy[0]] = arr[xy[1]];
+//             arr[xy[1]] = temp; 
+//         }
+//         return arr;
+//     }
+// }
+
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        // Declare temporary int value for swapping
-        int temp;
-        for (int[] xy : queries) {
-            // swap the selected values
-            temp = arr[xy[0]];
-            arr[xy[0]] = arr[xy[1]];
-            arr[xy[1]] = temp; 
+    public int[] solution (int[] arr, int[][] queries) {
+        
+        for (int i = 0; i < queries.length; i++) {
+            
+            int n = queries[i][0];
+            int m = queries[i][1];
+            
+            int temp = 0;
+            temp = arr[n];
+            arr[n] = arr[m];
+            arr[m] = temp;
+            
         }
+        
         return arr;
+        
     }
 }
