0ce19f0c0606ec1ae5f8f6db5ec52316834fbb7d
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-08 15:53:29 +0900

    [level 0] Title: 수 조작하기 2, Time: 9.75 ms, Memory: 82.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ Java/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,51 +1,20 @@
-// class Solution {
-//     public String solution(int[] numLog) {
-        
-//         // Use StringBuilder instead of String as String is immutalbe whcih means it creates new string every time in concatenates. 
-//         StringBuilder answer  = new StringBuilder();
-
-//         for (int i = 0; i < numLog.length - 1; i++) {
-//             switch (numLog[i+1] - numLog[i]) {
-//                 case 1 -> answer.append('w');
-//                 case 10 -> answer.append('d');
-//                 case -1 -> answer.append('s');
-//                 case -10 -> answer.append('a');
-//                 default -> {
-//                 }
-//             }
-//         }
-//         // Change StringBuilder to String.
-//         return answer.toString();
-//     }
-// }
-
 class Solution {
-    public String solution (int[] numLog) {
+    public String solution(int[] numLog) {
         
-        StringBuilder sb = new StringBuilder();
-        
-        int len = numLog.length;
-        
-        for (int i = 0; i < len - 1; i++) {
+        // Use StringBuilder instead of String as String is immutalbe whcih means it creates new string every time in concatenates. 
+        StringBuilder answer  = new StringBuilder();
+
+        for (int i = 0; i < numLog.length - 1; i++) {
             switch (numLog[i+1] - numLog[i]) {
-                    case 1 -> sb.append('w');
-                    case 10 -> sb.append('d');
-                    case -1 -> sb.append('s');
-                    case -10 -> sb.append('a');
-                    default -> {}
+                case 1 -> answer.append('w');
+                case 10 -> answer.append('d');
+                case -1 -> answer.append('s');
+                case -10 -> answer.append('a');
+                default -> {
+                }
             }
         }
-        
-        return sb.toString();
+        // Change StringBuilder to String.
+        return answer.toString();
     }
 }
-
-
-
-
-
-
-
-
-
-
