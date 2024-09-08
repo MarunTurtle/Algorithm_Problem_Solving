fc5b3f3457ab0c07ab5d7640612851e920389a67
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-08 16:34:18 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.96 ms, Memory: 76.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ Java/프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,17 +1,40 @@
-class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        for (int[] query : queries) {
-            int s = query[0];
-            int e = query[1];
-            int k = query[2];
+// class Solution {
+//     public int[] solution(int[] arr, int[][] queries) {
+//         for (int[] query : queries) {
+//             int s = query[0];
+//             int e = query[1];
+//             int k = query[2];
 
-            for (int i = s; i <= e; i++) {
-                if (i % k == 0) {
-                    arr[i] += 1;
+//             for (int i = s; i <= e; i++) {
+//                 if (i % k == 0) {
+//                     arr[i] += 1;
+//                 }
+//             }
+//         }
+//         int[] answer = arr;
+//         return answer;
+//     }
+// }
+
+
+
+class Solution {
+    public int[] solution (int[] arr, int[][] queries) {
+        
+        int t = queries.length;
+        
+        for (int i = 0; i < t; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            
+            for (int j = s; j <= e; j++) {
+                if (j % k == 0) {
+                    arr[j] += 1;
                 }
             }
         }
-        int[] answer = arr;
-        return answer;
+        
+        return arr;
     }
 }
