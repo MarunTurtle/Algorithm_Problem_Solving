6278c78f7e08527a98d2aefaa8e5640723092aec
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-08 16:51:40 +0900

    [level 0] Title: 배열 만들기 2, Time: 17.02 ms, Memory: 77 MB -BaekjoonHub

--- Java/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ Java/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,39 +1,82 @@
 
+// import java.util.ArrayList;
+
+// class Solution {
+//     public int[] solution(int l, int r) {
+//         // Use an ArrayList to dynamically store valid numbers
+//         ArrayList<Integer> resultList = new ArrayList<>();
+
+//         // Loop through all the numbers that are l <= i <= r
+//         for (int i = l; i <= r; i++) {
+//             if (containsOnly5Or0(i)) {
+//                 resultList.add(i);
+//             }
+//         }
+
+//         // If no valid numbers were found, return an array with -1
+//         if (resultList.isEmpty()) {
+//             return new int[]{-1};
+//         }
+
+//         // Convert ArrayList to int[]
+//         int[] resultArray = new int[resultList.size()];
+//         for (int i = 0; i < resultList.size(); i++) {
+//             resultArray[i] = resultList.get(i);
+//         }
+
+//         return resultArray;
+//     }
+
+//     private boolean containsOnly5Or0(int num) {
+//         String numStr = Integer.toString(num);
+//         for (char c : numStr.toCharArray()) {
+//             if (c != '5' && c != '0') {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+// import java.util.ArrayList;
+
+// class Solution {
+//     public int[] solution(int l, int r) {
+
+//         ArrayList<Integer> list = new ArrayList<>();
+
+//         for (int i = 1; i < 64; i++) {
+//             int num = Integer.parseInt(Integer.toBinaryString(i)) * 5;
+//             if (l <= num && num <= r)
+//                 list.add(num);
+//         }
+
+//         return list.isEmpty() ? new int[] { -1 } : list.stream().mapToInt(i -> i).toArray();
+//     }
+// }
+
+
 import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int l, int r) {
-        // Use an ArrayList to dynamically store valid numbers
-        ArrayList<Integer> resultList = new ArrayList<>();
 
-        // Loop through all the numbers that are l <= i <= r
-        for (int i = l; i <= r; i++) {
-            if (containsOnly5Or0(i)) {
-                resultList.add(i);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i < Math.pow(2, 20); i++) {  // 2^20까지 탐색
+            int num = Integer.parseInt(Integer.toBinaryString(i)) * 5;
+            if (num > r) {
+                break;  // num이 r을 넘어가면 반복문 종료
+            }
+            if (l <= num && num <= r) {
+                list.add(num);  // num이 l과 r 범위에 있을 때 추가
             }
         }
 
-        // If no valid numbers were found, return an array with -1
-        if (resultList.isEmpty()) {
-            return new int[]{-1};
-        }
-
-        // Convert ArrayList to int[]
-        int[] resultArray = new int[resultList.size()];
-        for (int i = 0; i < resultList.size(); i++) {
-            resultArray[i] = resultList.get(i);
-        }
-
-        return resultArray;
-    }
-
-    private boolean containsOnly5Or0(int num) {
-        String numStr = Integer.toString(num);
-        for (char c : numStr.toCharArray()) {
-            if (c != '5' && c != '0') {
-                return false;
-            }
-        }
-        return true;
+        return list.isEmpty() ? new int[] { -1 } : list.stream().mapToInt(i -> i).toArray();
     }
 }
+
+
+
