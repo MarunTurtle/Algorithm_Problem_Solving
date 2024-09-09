0d780be4af8ac3fff7e1cbcee713a672825593ac
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-09 15:53:51 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.86 ms, Memory: 75.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,25 +1,25 @@
-class Solution {
-    public String solution(String my_string, int[][] queries) {
-        StringBuilder sb = new StringBuilder(my_string);
+// class Solution {
+//     public String solution(String my_string, int[][] queries) {
+//         StringBuilder sb = new StringBuilder(my_string);
         
-        // Read every query and flip the selected segment of my_string
-        for (int[] query : queries) {
-            int start = query[0];
-            int end = query[1];
+//         // Read every query and flip the selected segment of my_string
+//         for (int[] query : queries) {
+//             int start = query[0];
+//             int end = query[1];
             
-            // Extract the substring to be flipped
-            String segment = sb.substring(start, end + 1);
+//             // Extract the substring to be flipped
+//             String segment = sb.substring(start, end + 1);
             
-            // Reverse the extracted substring
-            String reversedSegment = new StringBuilder(segment).reverse().toString();
+//             // Reverse the extracted substring
+//             String reversedSegment = new StringBuilder(segment).reverse().toString();
             
-            // Replace the original segment with the reversed segment
-            sb.replace(start, end + 1, reversedSegment);
-        }
+//             // Replace the original segment with the reversed segment
+//             sb.replace(start, end + 1, reversedSegment);
+//         }
         
-        return sb.toString();
-    }
-}
+//         return sb.toString();
+//     }
+// }
 
 // class Solution {
 
@@ -44,3 +44,30 @@ class Solution {
 //         }
 //     }
 // }
+
+
+import java.util.*;
+
+class Solution {
+    
+    char[] arr;
+    
+    public String solution (String my_string, int[][] queries) {
+        
+        arr = my_string.toCharArray();
+        
+        for (int[] query : queries){
+            reverse(query[0], query[1]);
+        }
+        
+        return new String (arr);
+    }
+    
+    private void reverse(int s, int e) {
+        while (s < e) {
+            char temp = arr[s];
+            arr[s++] = arr[e];
+            arr[e--] = temp;
+        }
+    }
+}
