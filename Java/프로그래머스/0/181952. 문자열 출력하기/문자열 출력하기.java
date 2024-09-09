a8202fc8d0c3c33f2435b2f5af7b11ffa042b606
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-09 16:18:21 +0900

    [level 0] Title: 문자열 출력하기, Time: 160.07 ms, Memory: 61.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ Java/프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -3,6 +3,6 @@ import java.util.*;
 public class Solution {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print(sc.nextLine());
+        System.out.println(sc.next());
     }
 }
