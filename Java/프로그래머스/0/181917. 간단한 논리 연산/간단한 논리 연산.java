c047c93f2f6d6f63cb39717c6c7020d72cb8be50
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-09 12:22:21 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 70.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ Java/프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,8 +1,8 @@
-class Solution {
-    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        return ((x1 || x2) && (x3 || x4));
-    }
-}
+// class Solution {
+//     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+//         return ((x1 || x2) && (x3 || x4));
+//     }
+// }
 
 
 // class Solution {
@@ -33,3 +33,12 @@ class Solution {
 //         return answer;
 //     }
 // }
+
+
+class Solution {
+    public boolean solution (boolean x1, boolean x2, boolean x3, boolean x4) {
+        
+        return ((x1 || x2)&&(x3 || x4));
+        
+    }
+}
