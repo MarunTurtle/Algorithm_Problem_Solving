080f0c246442f6c18202769784ba8727511c72e0
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-09 16:55:38 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 73.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,15 +1,15 @@
 public class Solution {
     public String solution (String str1, String str2) {
         
-        StringBuilder result = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         
         int len = str1.length();
+        
         for (int i = 0; i < len; i++) {
-            result.append(str1.charAt(i));     
-            result.append(str2.charAt(i));
+            sb.append(str1.charAt(i));
+            sb.append(str2.charAt(i));
         }
         
-        return result.toString();
-        
+        return sb.toString();
     }
 }
