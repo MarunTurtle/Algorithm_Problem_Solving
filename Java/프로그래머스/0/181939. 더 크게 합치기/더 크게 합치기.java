d08ce25875b5dfc713390736ae76e9f9acd134ef
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-09 17:06:00 +0900

    [level 0] Title: 더 크게 합치기, Time: 14.98 ms, Memory: 70 MB -BaekjoonHub

--- Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -2,11 +2,10 @@ import java.util.*;
 
 public class Solution {
     public int solution (int a, int b) {
-     
+        
         int m = Integer.parseInt("" + a + b);
         int n = Integer.parseInt("" + b + a);
         
-        return m > n ? m : n; 
-        
+        return m > n ? m : n;
     }
 }
