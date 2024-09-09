2f287fda78472ae6007c4b14b992eb4595e4186f
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-09 16:04:54 +0900

    [level 0] Title: 배열 만들기 5, Time: 10.71 ms, Memory: 88.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ Java/프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,31 +1,55 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
+
+// class Solution {
+//     public int[] solution(String[] intStrs, int k, int s, int l) {
+        
+//         ArrayList<Integer> resultArray = new ArrayList<>();
+
+
+//         for (String strNum : intStrs) {
+
+//             StringBuilder sb = new StringBuilder();
+
+//             for (int i = s; i < s + l; i++) {
+//                 sb.append(strNum.charAt(i));
+//             }
+
+//             int num = Integer.parseInt(sb.toString());
+            
+//             if (num > k) {
+//                 resultArray.add(num);
+//             }
+//         }
+        
+//         int[] answer = new int[resultArray.size()];
+//         for (int i = 0; i < resultArray.size(); i++) {
+//             answer[i] = resultArray.get(i);
+//         }
+        
+//         return answer;
+//     }
+// }
+
+import java.util.*;
 
 class Solution {
-    public int[] solution(String[] intStrs, int k, int s, int l) {
+
+    public int[] solution (String[] intStrs, int k, int s, int l) {
         
-        ArrayList<Integer> resultArray = new ArrayList<>();
-
-
-        for (String strNum : intStrs) {
-
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = s; i < s + l; i++) {
-                sb.append(strNum.charAt(i));
-            }
-
-            int num = Integer.parseInt(sb.toString());
+        ArrayList<Integer> resultArr = new ArrayList<>();
+        
+        for (String str : intStrs) {
+            String substr = str.substring(s, s + l);
+            int intSubStr = Integer.parseInt(substr);
             
-            if (num > k) {
-                resultArray.add(num);
+            if (intSubStr > k) {
+                resultArr.add(intSubStr);
             }
         }
         
-        int[] answer = new int[resultArray.size()];
-        for (int i = 0; i < resultArray.size(); i++) {
-            answer[i] = resultArray.get(i);
-        }
-        
-        return answer;
+        return resultArr.stream().mapToInt(i -> i).toArray();
     }
 }
+
+
+
