fc832558fa94b98dcb6553f4bb0273cbd8b3c55b
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-10 16:51:50 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.07 ms, Memory: 69.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ Java/프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,19 +1,53 @@
-class Solution {
-    public int solution(String my_string, String is_suffix) {
-        // Initial value for answer is false
-        int answer = 0;
-        
-        // Iterate through the substrings and compare with is_suffix
-        for (int i = 0; i < my_string.length(); i++) {
-            if (my_string.substring(i).equals(is_suffix)) {
-                answer = 1; 
-                break;
-            }
-        }
+import java.util.*;
 
-        return answer; 
+public class Solution {
+    public int solution(String my_string, String is_suffix) {
+        
+        return my_string.endsWith(is_suffix) ? 1 : 0;
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int solution(String my_string, String is_suffix) {
+//         // Initial value for answer is false
+//         int answer = 0;
+        
+//         // Iterate through the substrings and compare with is_suffix
+//         for (int i = 0; i < my_string.length(); i++) {
+//             if (my_string.substring(i).equals(is_suffix)) {
+//                 answer = 1; 
+//                 break;
+//             }
+//         }
+
+//         return answer; 
+//     }
+// }
 
 // class Solution {
 //     public int solution(String myString, String isSuffix) {
