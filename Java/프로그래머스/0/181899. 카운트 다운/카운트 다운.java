6a6e1e263b52f514dbb0da21a0ee1b0ac8e092d9
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-10 17:25:57 +0900

    [level 0] Title: 카운트 다운, Time: 3.30 ms, Memory: 77.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ Java/프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,11 +1,14 @@
+import java.util.*;
+
 class Solution {
-    public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[start_num - end_num + 1];
+    public int[] solution(int s, int e) {
         
-        for (int i = start_num; i >= end_num; i--) {
-            answer[start_num - i] = i;  
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for (int i = s; i >= e; i--) {
+            result.add(i);
         }
         
-        return answer;
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
