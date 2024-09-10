2dc51f2cd0b9bbf84dd8aa3c7cd926b739839f3d
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-10 17:28:44 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 81.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ Java/프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,14 +1,28 @@
-class Solution {
+// class Solution {
+//     public int solution(int[] arr, int idx) {
+        
+//         // Iterate through arry from idx to the end
+//         for (int j = idx; j < arr.length; j++) {
+//             // Check if the value of the item is 1
+//             if (arr[j] == 1) {
+//                 return j;
+//             }            
+//         }
+//         // If not found, return -1
+//         return -1;
+//     }
+// }
+
+import java.util.*;
+
+public class Solution {
     public int solution(int[] arr, int idx) {
         
-        // Iterate through arry from idx to the end
-        for (int j = idx; j < arr.length; j++) {
-            // Check if the value of the item is 1
-            if (arr[j] == 1) {
-                return j;
-            }            
+        for (int i = idx; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                return i;
+            } 
         }
-        // If not found, return -1
-        return -1;
+        return -1;    
     }
 }
