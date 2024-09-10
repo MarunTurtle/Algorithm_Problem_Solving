11b8b5757d36fa4dd0f33e11bbc23a59fbe3337a
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-10 17:10:15 +0900

    [level 0] Title: 배열 만들기 1, Time: 4.04 ms, Memory: 85.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ Java/프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,17 +1,34 @@
+// class Solution {
+//     public int[] solution(int n, int k) {
+        
+//         // Calculate the size of the answer array
+//         int size = n / k;
+        
+//         // Initialize the answer array with the correct size
+//         int[] answer = new int[size];
+       
+//         // Fill the array with multiples of k
+//         for (int i = 1; i <= size; i++) {
+//             answer[i - 1] = i * k;
+//         }
+        
+//         return answer;
+//     }
+// }
+
+import java.util.*;
+
 class Solution {
     public int[] solution(int n, int k) {
         
-        // Calculate the size of the answer array
-        int size = n / k;
+        ArrayList<Integer> result = new ArrayList<>();
         
-        // Initialize the answer array with the correct size
-        int[] answer = new int[size];
-       
-        // Fill the array with multiples of k
-        for (int i = 1; i <= size; i++) {
-            answer[i - 1] = i * k;
+        for (int i = 0; i <= n; i+=k) {   
+            if (i != 0) {
+                result.add(i);
+            }
         }
         
-        return answer;
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
