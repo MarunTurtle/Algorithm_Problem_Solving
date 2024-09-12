9b06220fb897f0a46ec7c62fda751f31d6c34498
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-12 12:48:47 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.06 ms, Memory: 71.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ Java/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,6 +1,20 @@
-class Solution {
-    public int solution(String my_string, String is_prefix) {
+// class Solution {
+//     public int solution(String my_string, String is_prefix) {
                 
-        return my_string.startsWith(is_prefix) ? 1 : 0;
+//         return my_string.startsWith(is_prefix) ? 1 : 0;
+//     }
+// }
+
+class Solution {
+    public int solution (String my_string, String is_prefix) {
+        
+        int len = my_string.length();
+        
+        for (int i = 0; i < len; i++) {
+            if (is_prefix.equals(my_string.substring(0, i))) {
+                return 1;
+            }
+        }
+        return 0;
     }
 }
