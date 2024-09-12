f83c799f3f0530c6aa42a34c5ea7374165660194
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-12 15:42:24 +0900

    [level 0] Title: 문자열 뒤집기, Time: 18.98 ms, Memory: 75 MB -BaekjoonHub

--- Java/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ Java/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,14 +1,13 @@
 class Solution {
-    public String solution(String my_string, int s, int e) {
-        // Extract the substring that needs to be reversed
-        StringBuilder string = new StringBuilder(my_string.substring(s, e + 1));
-       
-        // Reverse the substring
-        string.reverse();
-
-        // Build the final string
-        String result = my_string.substring(0, s) + string.toString() + my_string.substring(e + 1);
+    public String solution (String my_string, int s, int e) {
         
-        return result;
+        StringBuilder substr = new StringBuilder(my_string.substring(s, e+1));
+        
+        String reversed = substr.reverse().toString();
+        
+        StringBuilder result = new StringBuilder(my_string.substring(0, s) + reversed + my_string.substring(e+1));
+        
+        return result.toString();
+        
     }
 }
