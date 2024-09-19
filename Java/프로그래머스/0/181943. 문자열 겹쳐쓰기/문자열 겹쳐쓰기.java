e2ca7fa50cd1ace317fedf69fb2924df1d11d758
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-19 21:46:50 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.11 ms, Memory: 73.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ Java/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,29 +1,3 @@
-// public class Solution {
-//     public String solution (String my_string, String overwrite_string, int s) {
-        
-//         StringBuilder sb = new StringBuilder(my_string);
-        
-//         int len = overwrite_string.length();
-        
-//         sb.replace(s, s+len, overwrite_string);
-        
-//         return sb.toString();
-//     }
-// }
-
-
-// public class Solution {
-//     public String solution (String my_string, String overwrite_string, int s) {
-//         StringBuilder sb = new StringBuilder(my_string);
-        
-//         sb.replace(s, s+overwrite_string.length(), overwrite_string);
-        
-//         return sb.toString();
-        
-        
-//     }
-// }
-
 public class Solution {
     public String solution (String my_string, String overwrite_string, int s) {
         
@@ -32,5 +6,6 @@ public class Solution {
         sb.replace(s, s+overwrite_string.length(), overwrite_string);
         
         return sb.toString();
+        
     }
 }
