8b1e63e3120830f1619d5b69c625cecae8a7622b
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-21 21:13:32 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 5.93 ms, Memory: 82.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ Java/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,5 +1,36 @@
+// import java.util.*;
+// import java.util.stream.*;
+
+// public class Solution {
+//     public int[] solution(int[] arr, int[][] queries) {
+        
+//         ArrayList<Integer> result = new ArrayList<>();
+        
+//         for (int[] query : queries) {
+            
+//             int min = Integer.MAX_VALUE;
+//             int s = query[0];
+//             int e = query[1];
+//             int k = query[2];
+            
+//             for (int i = s; i <= e; i++) {
+//                 if (arr[i] > k && arr[i] < min) {
+//                     min = arr[i];     
+//                 }                    
+//             }
+            
+//             if (min == Integer.MAX_VALUE) {
+//                 result.add(-1);
+//             } else {
+//                 result.add(min);
+//             }
+//         }
+        
+//         return result.stream().mapToInt(i -> i).toArray();
+//     }
+// }
+
 import java.util.*;
-import java.util.stream.*;
 
 public class Solution {
     public int[] solution(int[] arr, int[][] queries) {
@@ -7,25 +38,39 @@ public class Solution {
         ArrayList<Integer> result = new ArrayList<>();
         
         for (int[] query : queries) {
-            
-            int min = Integer.MAX_VALUE;
             int s = query[0];
             int e = query[1];
             int k = query[2];
+            int min = Integer.MAX_VALUE;
             
             for (int i = s; i <= e; i++) {
                 if (arr[i] > k && arr[i] < min) {
-                    min = arr[i];     
-                }                    
+                    min = arr[i];
+                }
             }
             
-            if (min == Integer.MAX_VALUE) {
-                result.add(-1);
-            } else {
-                result.add(min);
-            }
+            min = min == Integer.MAX_VALUE ? -1 : min;
+            
+            result.add(min);
         }
         
         return result.stream().mapToInt(i -> i).toArray();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
