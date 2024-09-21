21f058f702ff67d85da957395a2bcf4c5ce12986
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-21 21:45:20 +0900

    [level 0] Title: 배열 만들기 2, Time: 87.25 ms, Memory: 103 MB -BaekjoonHub

--- Java/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ Java/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,64 +1,5 @@
 
 // import java.util.ArrayList;
-
-// class Solution {
-//     public int[] solution(int l, int r) {
-//         // Use an ArrayList to dynamically store valid numbers
-//         ArrayList<Integer> resultList = new ArrayList<>();
-
-//         // Loop through all the numbers that are l <= i <= r
-//         for (int i = l; i <= r; i++) {
-//             if (containsOnly5Or0(i)) {
-//                 resultList.add(i);
-//             }
-//         }
-
-//         // If no valid numbers were found, return an array with -1
-//         if (resultList.isEmpty()) {
-//             return new int[]{-1};
-//         }
-
-//         // Convert ArrayList to int[]
-//         int[] resultArray = new int[resultList.size()];
-//         for (int i = 0; i < resultList.size(); i++) {
-//             resultArray[i] = resultList.get(i);
-//         }
-
-//         return resultArray;
-//     }
-
-//     private boolean containsOnly5Or0(int num) {
-//         String numStr = Integer.toString(num);
-//         for (char c : numStr.toCharArray()) {
-//             if (c != '5' && c != '0') {
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-// }
-
-
-// import java.util.ArrayList;
-
-// class Solution {
-//     public int[] solution(int l, int r) {
-
-//         ArrayList<Integer> list = new ArrayList<>();
-
-//         for (int i = 1; i < 64; i++) {
-//             int num = Integer.parseInt(Integer.toBinaryString(i)) * 5;
-//             if (l <= num && num <= r)
-//                 list.add(num);
-//         }
-
-//         return list.isEmpty() ? new int[] { -1 } : list.stream().mapToInt(i -> i).toArray();
-//     }
-// }
-
-
-import java.util.*;
-
 // public class Solution {
 //     public int[] solution (int l, int r) {
         
@@ -69,29 +10,37 @@ import java.util.*;
 //             int num = Integer.parseInt(Integer.toBinaryString(i)) * 5;
             
 //             if (l <= num && num <= r) list.add(num);
-
+            
 //         }
         
-//         return list.isEmpty() ? new int[] {-1} : list.stream().mapToInt(i -> i).toArray();
-        
+//         return list.isEmpty() ? new int[] {-1} : list.stream().mapToInt(i -> i).toArray();  
 //     }
 // }
 
-
+import java.util.*;
 
 public class Solution {
     public int[] solution (int l, int r) {
         
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
         
-        for (int i = 1; i < 64; i++) {
+        for (int i = l; i <= r; i++) {
+            String num = "" + i;
+            boolean check = true;
             
-            int num = Integer.parseInt(Integer.toBinaryString(i)) * 5;
-            
-            if (l <= num && num <= r) list.add(num);
-            
+            for (int j = 0; j < num.length(); j++) {
+                if (num.charAt(j) != '5' && num.charAt(j) != '0') {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                result.add(i);   
+            }
         }
         
-        return list.isEmpty() ? new int[] {-1} : list.stream().mapToInt(i -> i).toArray();  
+        return result.isEmpty() ? new int[] {-1} : result.stream().mapToInt(i -> i).toArray();
+        
     }
 }
+
