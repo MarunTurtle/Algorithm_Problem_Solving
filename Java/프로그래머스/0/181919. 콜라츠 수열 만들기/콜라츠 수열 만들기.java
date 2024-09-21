e9ee4a530e5b7c7b84214cc26d1dbebdfe83d025
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-21 21:55:28 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 2.94 ms, Memory: 73.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ Java/프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -20,93 +20,47 @@
 //     }
 // }
 
+
 // import java.util.*;
 
-// class Solution {
+// public class Solution {
 //     public int[] solution (int n) {
-        
-//         ArrayList<Integer> list = new ArrayList<>();
+    
+//         ArrayList<Integer> result = new ArrayList<>();
         
 //         while (n != 1) {
-            
-//             list.add(n);
+//             result.add(n);
 //             if (n % 2 == 0) {
 //                 n /= 2;
 //             } else {
-//                 n = n * 3 + 1;
+//                 n = 3 * n + 1;
 //             }
 //         }
         
-//         list.add(1);
-        
-//         return list.stream().mapToInt(i -> i).toArray();
+//         result.add(1);
+//         return result.stream().mapToInt(i -> i).toArray();
 //     }
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import java.util.LinkedList; // Import LinkedList to use it as a Queue
-// import java.util.Queue; // Import Queue interface
-
-// class Solution {
-//     public int[] solution(int n) {
-//         // Initialize a Queue to store the Collatz sequence
-//         Queue<Integer> answer = new LinkedList<>();
-        
-//         // Loop to generate the Collatz sequence
-//         while (n > 1) {
-//             // Add the current value of n to the queue
-//             answer.add(n);
-//             // If n is even, divide it by 2 using the bitwise right shift operator
-//             if (n % 2 == 0) {
-//                 n >>= 1;
-//             // If n is odd, apply the 3n + 1 rule
-//             } else {
-//                 n = n * 3 + 1;
-//             }
-//         }
-        
-//         // Add the final value 1 to the queue (since all Collatz sequences end at 1)
-//         answer.add(1);
-        
-//         // Convert the Queue to an int[] array and return it
-//         return answer.stream().mapToInt(i -> i).toArray();
-//     }
-// }
-
 
 import java.util.*;
 
 public class Solution {
     public int[] solution (int n) {
-    
+        
         ArrayList<Integer> result = new ArrayList<>();
         
         while (n != 1) {
-            result.add(n);
             if (n % 2 == 0) {
+                result.add(n);
                 n /= 2;
             } else {
+                result.add(n);
                 n = 3 * n + 1;
             }
         }
         
         result.add(1);
         return result.stream().mapToInt(i -> i).toArray();
+        
     }
 }
-
-
-
-
-
