0c78364e8282ad1a1e81566388c5306a9c640935
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-23 07:59:03 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 77.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ Java/프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,9 +1,16 @@
 class Solution {
-    public int solution(int[] num_list) {
+    public int solution (int[] num_list) {
         
-        for (int i = 0; i < num_list.length; i++) {         
-            if (num_list[i] < 0) return i;
+        int result = -1;
+        int len = num_list.length;
+        
+        for (int i = 0; i < len; i++) {
+            if (num_list[i] < 0) {
+                result = i;
+                break;
+            }
         }
-        return -1;
+        
+        return result;
     }
 }
