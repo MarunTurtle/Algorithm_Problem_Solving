d5143c2335962aec796e0345c20272255d8c51da
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-22 21:42:34 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
+++ Java/프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -1,11 +1,9 @@
-class Solution {
+public class Solution {
     public String solution (String my_string, int n) {
-        
-        StringBuilder result = new StringBuilder(my_string);
         
         int len = my_string.length();
         
-        return result.substring(len-n).toString();      
+        return my_string.substring(len - n);
         
     }
 }
