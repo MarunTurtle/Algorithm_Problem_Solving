c397b3a847305864e0f6b8e142b2a62e3023563c
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-23 08:36:58 +0900

    [level 0] Title: 2의 영역, Time: 14.51 ms, Memory: 95.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ Java/프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,40 +1,31 @@
-// class Solution {
-//     public int[] solution(int[] arr) {
-        
-//         int startIdx = Integer.MAX_VALUE; 
-//         int endIdx = Integer.MIN_VALUE;
-//         int size = arr.length;
-        
-//         for (int i = 0; i < size; i++) {
-//             if (arr[i] == 2) {
-//                 if (startIdx > i) {
-//                     startIdx = i;
-//                 }
-//                 endIdx = i;
-//             }
-//         }
-        
-//         if (endIdx == Integer.MIN_VALUE) endIdx = startIdx;
-//         if (startIdx == Integer.MAX_VALUE) {
-//             int[] answer = new int[1];
-//             answer[0] = -1;
-//             return answer;
-//         }
-            
-//         int[] answer = new int[endIdx - startIdx + 1];
-//         for (int j = startIdx; j <= endIdx; j++) {
-//             answer[j - startIdx] = arr[j];
-//         }
-//         return answer;
-//     }
-// }
-
 import java.util.*;
-import java.util.stream.Collectors;
 
-class Solution {
-    public List<Integer> solution(int[] arr) {
-        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
-        return list.contains(2) ? list.subList(list.indexOf(2), list.lastIndexOf(2) + 1) : Arrays.asList(-1);
+public class Solution {
+    public int[] solution (int[] arr) {
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        int s = -1;  
+        int e = -1;
+        int len = arr.length;
+        
+        for (int i = 0; i < len; i++) {
+            if (arr[i] == 2) {
+                if (s == -1) {
+                    s = i;  
+                }
+                e = i;  
+            }
+        }
+        
+        if (s == -1) {
+            return new int[]{-1};
+        }
+        
+        for (int j = s; j <= e; j++) {
+            result.add(arr[j]);
+        }     
+        
+        return result.stream().mapToInt(i -> i).toArray();                
     }
 }
