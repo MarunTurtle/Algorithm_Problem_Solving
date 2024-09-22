e217ca99717727041822701104f6e1e5bd14c3ca
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-22 22:15:19 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.12 ms, Memory: 75.5 MB -BaekjoonHub

--- Java/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ Java/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,12 +1,14 @@
 public class Solution {
-    public String solution(String my_string, int s, int e) {
+    public String solution (String my_string, int s, int e) {
         
         StringBuilder result = new StringBuilder(my_string);
         
-        String reversed = new StringBuilder(result.substring(s, e+1)).reverse().toString();
+        String reversed = new StringBuilder(my_string.substring(s, e+1)).reverse().toString();
         
         result.replace(s, e+1, reversed);
         
         return result.toString();
+        
+        
     }
 }
