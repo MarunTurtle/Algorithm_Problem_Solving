3df44df74cb229e1e61ba9041be2fcd999ad9960
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-22 22:23:37 +0900

    [level 0] Title: qr code, Time: 0.80 ms, Memory: 85.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181903. qr code/qr code.java
+++ Java/프로그래머스/0/181903. qr code/qr code.java
@@ -1,12 +1,10 @@
-class Solution {
+public class Solution {
     public String solution (int q, int r, String code) {
         
         StringBuilder result = new StringBuilder();
         
-        for (int i = 0; i < code.length(); i++) {
-            if (i % q == r) {
-                result.append(code.charAt(i));
-            }
+        for (int i = r; i < code.length(); i += q) {
+            result.append(code.charAt(i));
         }
         
         return result.toString();
