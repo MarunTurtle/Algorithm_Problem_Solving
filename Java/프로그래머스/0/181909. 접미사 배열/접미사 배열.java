ed9c065d91157147aeb1300b71f723c473cbe2da
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-23 14:31:51 +0900

    [level 0] Title: 접미사 배열, Time: 3.92 ms, Memory: 78.5 MB -BaekjoonHub

--- Java/프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ Java/프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class Solution {
     public String[] solution (String my_string) {
         
-        ArrayList<String> result = new ArrayList<>();
-        
         int len = my_string.length();
+        
+        ArrayList<String> result = new ArrayList<>();
         
         for (int i = 0; i < len; i++) {
             result.add(my_string.substring(i));
         }
         
-        return result.stream().sorted().toArray(String[]::new);       
+        return result.stream().sorted().toArray(String[]::new);
     }
 }
