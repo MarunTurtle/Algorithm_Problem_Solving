8dcbfbbdd6965f52ce6cc2ab53b482e3e5fde59e
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-23 21:05:28 +0900

    [level 0] Title: 세로 읽기, Time: 0.26 ms, Memory: 79.5 MB -BaekjoonHub

--- Java/프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ Java/프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,28 +1,14 @@
-class Solution {
-    public String solution(String my_string, int m, int c) {
-        StringBuilder answer = new StringBuilder();
-        int length = my_string.length();
+import java.util.*;
 
-        // Extract characters where i % m == c - 1 (since c is 1-based index)
-        for (int i = 0; i < length; i++) {
-            if (i % m == c - 1) {
-                answer.append(my_string.charAt(i));
-            }
+public class Solution {
+    public String solution (String my_string, int m, int c) {
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            result.append(my_string.charAt(i));
         }
-
-        return answer.toString();
+        
+        return result.toString();
     }
 }
-
-
-// class Solution {
-
-//     public String solution(String my_string, int m, int c) {
-//         String answer = "";
-
-//         for (int i = c - 1; i < my_string.length(); i += m) {
-//             answer += my_string.charAt(i);
-//         }
-//         return answer;
-//     }
-// }
