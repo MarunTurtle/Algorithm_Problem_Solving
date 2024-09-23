a647085003db66fdf8077e6bbf1f5adac38190a6
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-23 21:40:37 +0900

    [level 0] Title: 글자 지우기, Time: 0.17 ms, Memory: 75.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ Java/프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,49 +1,21 @@
-// class Solution {
-//     public String solution(String my_string, int[] indices) {
-//         // Delcare boolean[] to keep track of must-delete indices
-//         boolean[] toDelete = new boolean[my_string.length()];
-        
-//         // Set the indices that need deletion
-//         for (int index : indices) {
-//             toDelete[index] = true;
-//         }
-        
-//         // Declare Stringbuilder answer
-//         StringBuilder answer = new StringBuilder();
-        
-//         // Iterate through my_string
-//         for (int i = 0; i < my_string.length(); i++) {
-//             // If it is NOT TRUE to delete, append the chracter to the answer
-//             if (!toDelete[i]) {
-//                 answer.append(my_string.charAt(i));
-//             }
-//         }
-        
-//         return answer.toString();
-//     }
-// }
-
 import java.util.*;
 
 public class Solution {
     public String solution(String my_string, int[] indices) {
-        StringBuilder result = new StringBuilder();
-        char[] str = my_string.toCharArray();
-        
         Set<Integer> indexSet = new HashSet<>();
-        for (int index : indices) {
-            indexSet.add(index);
+        
+        for (int idx : indices) {
+            indexSet.add(idx);
         }
         
-        for (int i = 0; i < str.length; i++) {
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < my_string.length(); i++) {
             if (!indexSet.contains(i)) {
-                result.append(str[i]);
+                result.append(my_string.charAt(i));
             }
         }
         
         return result.toString();
     }
 }
-
-
-
