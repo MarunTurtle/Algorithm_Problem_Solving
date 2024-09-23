df5812fb0dcb6cda63985bb764bd2cea04c9c30b
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-23 14:12:35 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 3.72 ms, Memory: 82.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,17 +1,16 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public String solution (String my_string, int[][] queries) {
         
         StringBuilder result = new StringBuilder(my_string);
         
-        for (int[] query : queries) { 
+        for (int[] query : queries) {
             int s = query[0];
             int e = query[1];
             
-            StringBuilder substr = new StringBuilder(result.substring(s, e+1));
-            
-            result.replace(s, e+1, substr.reverse().toString());
+            StringBuilder subStr = new StringBuilder(result.substring(s, e+1));
+            result.replace(s, e+1, subStr.reverse().toString());
         }
         
         return result.toString();
