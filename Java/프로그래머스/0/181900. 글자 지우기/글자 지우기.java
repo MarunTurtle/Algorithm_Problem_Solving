ae283d754e9a7956fb8af4258cd173e9e5758806
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-23 21:43:18 +0900

    [level 0] Title: 글자 지우기, Time: 0.55 ms, Memory: 78.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ Java/프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,21 +1,44 @@
-import java.util.*;
+// import java.util.*;
 
-public class Solution {
+// public class Solution {
+//     public String solution(String my_string, int[] indices) {
+//         Set<Integer> indexSet = new HashSet<>();
+        
+//         for (int idx : indices) {
+//             indexSet.add(idx);
+//         }
+        
+//         StringBuilder result = new StringBuilder();
+        
+//         for (int i = 0; i < my_string.length(); i++) {
+//             if (!indexSet.contains(i)) {
+//                 result.append(my_string.charAt(i));
+//             }
+//         }
+        
+//         return result.toString();
+//     }
+// }
+
+// import java.util.*;
+
+
+
+// class Solution {
+//     public String solution(String my_string, int[] indices) {
+//         StringBuilder sb = new StringBuilder(my_string);
+//         for (int i : indices) {
+//             sb.setCharAt(i, ' ');
+//         }
+//         return sb.toString().replace(" ", "");
+//     }
+// }
+
+
+class Solution {
     public String solution(String my_string, int[] indices) {
-        Set<Integer> indexSet = new HashSet<>();
-        
-        for (int idx : indices) {
-            indexSet.add(idx);
-        }
-        
-        StringBuilder result = new StringBuilder();
-        
-        for (int i = 0; i < my_string.length(); i++) {
-            if (!indexSet.contains(i)) {
-                result.append(my_string.charAt(i));
-            }
-        }
-        
-        return result.toString();
+        String[] str = my_string.split("");
+        for (int i=0;i<indices.length;i++) str[indices[i]] = "";
+        return String.join("",str);
     }
 }
