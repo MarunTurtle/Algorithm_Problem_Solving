834c2bbab231d53838057f6de563bbcf4606ba9b
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-23 12:42:55 +0900

    [level 0] Title: 배열 만들기 4, Time: 23.52 ms, Memory: 80.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ Java/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,52 +1,29 @@
-// import java.util.*;
-
-// class Solution {
-//     public int[] solution (int[] arr) {
-        
-//         Deque<Integer> stack = new ArrayDeque<>();
-        
-//         for (int num : arr) {
-//             while (!stack.isEmpty() && stack.peek() >= num) {
-//                 stack.pop();
-//             }
-//             stack.push(num);
-//         }
-        
-//         return stack.stream().sorted().mapToInt(i -> i).toArray();
-//     }
-// 
-
 import java.util.*;
 
 public class Solution {
     public int[] solution (int[] arr) {
         
         Deque<Integer> stk = new ArrayDeque<>();
+        
+        for (int num : arr) {
             
-        for (int i = 0; i < arr.length; i++) {
-            
-            // Add first element if stack is empty
             if (stk.isEmpty()) {
-                stk.push(arr[i]);
-                continue;
+                stk.push(num);
             }
             
-            // Remove elements from the stack while they are less than or equal to the current element
-            while (!stk.isEmpty() && arr[i] <= stk.peek()) {
+            while (!stk.isEmpty() && num <= stk.peek()) {
                 stk.poll();
             }
             
-            // Add the current element to the stack
-            stk.push(arr[i]);
+            stk.push(num);
+
         }
         
-        // Return the stack elements as an array
-        // Note that the stack contains elements in reverse order, so we need to reverse it
         int[] result = new int[stk.size()];
-        int index = stk.size() - 1;
+        int idx = stk.size() - 1;
         
         for (int num : stk) {
-            result[index--] = num;
+            result[idx--] = num;
         }
         
         return result;
