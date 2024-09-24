fb067577f556801b370c674808a8093cbe000bd0
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-24 11:41:51 +0900

    [level 0] Title: 배열 만들기 5, Time: 12.16 ms, Memory: 102 MB -BaekjoonHub

--- Java/프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ Java/프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,55 +1,18 @@
-// import java.util.ArrayList;
-
-// class Solution {
-//     public int[] solution(String[] intStrs, int k, int s, int l) {
-        
-//         ArrayList<Integer> resultArray = new ArrayList<>();
-
-
-//         for (String strNum : intStrs) {
-
-//             StringBuilder sb = new StringBuilder();
-
-//             for (int i = s; i < s + l; i++) {
-//                 sb.append(strNum.charAt(i));
-//             }
-
-//             int num = Integer.parseInt(sb.toString());
-            
-//             if (num > k) {
-//                 resultArray.add(num);
-//             }
-//         }
-        
-//         int[] answer = new int[resultArray.size()];
-//         for (int i = 0; i < resultArray.size(); i++) {
-//             answer[i] = resultArray.get(i);
-//         }
-        
-//         return answer;
-//     }
-// }
-
 import java.util.*;
 
-class Solution {
-
+public class Solution {
     public int[] solution (String[] intStrs, int k, int s, int l) {
+               
+        ArrayList<Integer> result = new ArrayList<>();
         
-        ArrayList<Integer> resultArr = new ArrayList<>();
-        
-        for (String str : intStrs) {
-            String substr = str.substring(s, s + l);
-            int intSubStr = Integer.parseInt(substr);
+        for (String intStr : intStrs) {
             
-            if (intSubStr > k) {
-                resultArr.add(intSubStr);
-            }
+            int subint = Integer.parseInt(intStr.substring(s, s + l));
+            
+            if (subint > k) result.add(subint);
+            
         }
         
-        return resultArr.stream().mapToInt(i -> i).toArray();
-    }
+        return result.stream().mapToInt(i -> i).toArray();
+    }        
 }
-
-
-
