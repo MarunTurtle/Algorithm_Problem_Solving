da5475118abaf48aed03c1dc93ab30776099e84d
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-25 20:00:12 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.03 ms, Memory: 77.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ Java/프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,34 +1,12 @@
 import java.util.*;
 
-class Solution {
-    public int[] solution(int[] num_list, int n) {
-        // Using a list to dynamically store the result
-        List<Integer> resultList = new ArrayList<>();
+public class Solution {
+    public int[] solution (int[] nums, int n) {
         
-        // Iterate with step of n and add to the list
-        for (int i = 0; i < num_list.length; i += n) {
-            resultList.add(num_list[i]);
-        }
+        int[] result = new int[(nums.length - 1 + n)/n];
         
-        // Convert the list back to an array
-        return resultList.stream().mapToInt(Integer::intValue).toArray();
-    }
-}
-
-
-import java.util.*;
-
-class Solution {
-    public int[] solution(int[] num_list, int n) {
-        // Calculate the length of the resulting array
-        int length = (num_list.length + n - 1) / n;
-
-        // Create the result array with the calculated length
-        int[] result = new int[length];
-        
-        // Fill the result array by picking every n-th element
-        for (int i = 0, j = 0; i < num_list.length; i += n, j++) {
-            result[j] = num_list[i];
+        for (int i = 0, j = 0; i < nums.length; i += n) {
+            result[j++] = nums[i];
         }
         
         return result;
