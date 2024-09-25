6dd7d9c68c593ee3c18f588b06e492b191698a00
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-25 20:56:31 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.24 ms, Memory: 77.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ Java/프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,6 +1,15 @@
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        return answer;
+    public int[] solution (int[] arr, int[][] queries) {
+        
+        for (int[] query : queries) {
+            int s = query[0];
+            int e = query[1];
+            
+            for (int i = s; i <= e; i++) {
+                arr[i] += 1;
+            }
+        }
+        
+        return arr;
     }
 }
