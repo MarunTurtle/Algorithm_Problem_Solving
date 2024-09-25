70177fd7b19ef2256ee4e5f2dcc46af45c291e3c
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-25 15:50:55 +0900

    [level 0] Title: 리스트 자르기, Time: 0.03 ms, Memory: 72 MB -BaekjoonHub

--- Java/프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ Java/프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,34 +1,18 @@
+import java.util.*;
+
 public class Solution {
     public int[] solution (int n, int[] slicer, int[] num_list) {
-        int a = slicer[0];
-        int b = slicer[1];
-        int c = slicer[2];
-        int len = num_list.length - 1;
         
-        int start = n == 1 ? 0 : a;
-        int end = n == 2 ? len : b;
-        int steps = n == 4 ? c : 1;
+        int s = n == 1 ? 0 : slicer[0];
+        int e = n == 2 ? num_list.length - 1 : slicer[1];
+        int k = n == 4 ? slicer[2] : 1;
         
-        int[] result = new int[(end - start + steps) / steps];
-        int idx = 0;
-            
-        for (int i = start; i <= end; i += steps) {
-            result[idx++] = num_list[i];
+        int[] result = new int[(e - s + k)/k];
+        
+        for (int i = s, j = 0; i <= e; i += k) {
+            result[j++] = num_list[i];
         }
         
-        return result;        
+        return result;
     }
 }
-
-// class Solution {
-//     public int[] solution(int n, int[] slicer, int[] num_list) {
-//         int start = n == 1 ? 0 : slicer[0];
-//         int end = n == 2 ? num_list.length - 1 : slicer[1];
-//         int step = n == 4 ? slicer[2] : 1;
-//         int[] answer = new int[(end - start + step) / step];
-//         for (int i = start, j = 0; i <= end; i += step) {
-//             answer[j++] = num_list[i];
-//         }
-//         return answer;
-//     }
-// }
