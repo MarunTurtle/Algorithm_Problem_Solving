886844b9e4de8a881ef2ae22048b03abb7bed1da
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-25 19:43:32 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 65.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181890. 왼쪽 오른쪽/README.md
+++ Java/프로그래머스/0/181890. 왼쪽 오른쪽/README.md
@@ -1,10 +1,10 @@
 # [level 0] 왼쪽 오른쪽 - 181890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 0.05 ms
+메모리: 65.2 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 27일 20:55:58
+2024년 09월 25일 19:43:30
 
 ### 문제 설명
 
--- Java/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ Java/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,6 +1,19 @@
-class Solution {
-    public String[] solution(String[] str_list) {
-        String[] answer = {};
-        return answer;
+import java.util.*;
+
+public class Solution {
+    public String[] solution (String[] str_list) {
+        
+        for (int i = 0; i < str_list.length; i++) {
+            if (str_list[i].equals("l")) {
+                
+                return Arrays.copyOfRange(str_list, 0, i);
+            
+            } else if (str_list[i].equals("r")) {
+                
+                return Arrays.copyOfRange(str_list, i+1, str_list.length);
+            }
+        }
+        
+        return new String[]{};
     }
 }
