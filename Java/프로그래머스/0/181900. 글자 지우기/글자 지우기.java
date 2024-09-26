bdd93ad7f44c4b5a90e6608d6c34a814f3cc135d
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-26 10:37:56 +0900

    [level 0] Title: 글자 지우기, Time: 1.36 ms, Memory: 80 MB -BaekjoonHub

--- Java/프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ Java/프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,65 +1,12 @@
-// // import java.util.*;
-
-// // public class Solution {
-// //     public String solution(String my_string, int[] indices) {
-// //         Set<Integer> indexSet = new HashSet<>();
+class Solution {
+    public String solution (String str, int[] idxs) {
         
-// //         for (int idx : indices) {
-// //             indexSet.add(idx);
-// //         }
+        String[] arr = str.split("");
         
-// //         StringBuilder result = new StringBuilder();
-        
-// //         for (int i = 0; i < my_string.length(); i++) {
-// //             if (!indexSet.contains(i)) {
-// //                 result.append(my_string.charAt(i));
-// //             }
-// //         }
-        
-// //         return result.toString();
-// //     }
-// // }
-
-// // import java.util.*;
-
-
-
-// // class Solution {
-// //     public String solution(String my_string, int[] indices) {
-// //         StringBuilder sb = new StringBuilder(my_string);
-// //         for (int i : indices) {
-// //             sb.setCharAt(i, ' ');
-// //         }
-// //         return sb.toString().replace(" ", "");
-// //     }
-// // }
-
-// class Solution {
-//     public String solution (String my_string, int[] indices) {
-        
-//         String[] result = my_string.split("");
-        
-//         for (int i : indices) {
-//             result[i] = "";
-//         }
-        
-//         return String.join("", result);
-//     }
-// }
-
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-public class Solution {
-    public String solution(String my_string, int[] indices) {
-        Set<Integer> indexSet = Arrays.stream(indices).boxed().collect(Collectors.toSet());
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < my_string.length(); i++) {
-            if (!indexSet.contains(i)) {
-                result.append(my_string.charAt(i));
-            }
+        for (int idx : idxs) {
+            arr[idx] = "";
         }
-        return result.toString();
+        
+        return String.join("", arr);
     }
 }
