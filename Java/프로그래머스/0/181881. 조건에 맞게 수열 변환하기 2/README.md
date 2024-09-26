fc517d2dc1836da8858af6181c891804134c0b69
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-26 17:56:44 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 46.49 ms, Memory: 129 MB -BaekjoonHub

--- Java/프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/README.md
+++ Java/프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 조건에 맞게 수열 변환하기 2 - 181881 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181881?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181881) 
 
 ### 성능 요약
 
-메모리: 122 MB, 시간: 42.35 ms
+메모리: 129 MB, 시간: 46.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 28일 07:59:12
+2024년 09월 26일 17:56:41
 
 ### 문제 설명
 
--- Java/프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ Java/프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -1,29 +1,28 @@
-class Solution {
+public class Solution {
     public int solution(int[] arr) {
-        int count = 0;
+        int unchanged; 
+        int count = 0; 
         
         while (true) {
-            boolean changed = false;  // Track if any change occurs in the current iteration
+            unchanged = 0; 
             
             for (int i = 0; i < arr.length; i++) {
-                // Check if the element needs to be modified
                 if (arr[i] >= 50 && arr[i] % 2 == 0) {
-                    arr[i] /= 2;  // If even and >= 50, divide by 2
-                    changed = true;  // A change occurred
+                    arr[i] /= 2;
                 } else if (arr[i] < 50 && arr[i] % 2 == 1) {
-                    arr[i] = arr[i] * 2 + 1;  // If odd and < 50, multiply by 2 and add 1
-                    changed = true;  // A change occurred
+                    arr[i] = arr[i] * 2 + 1;
+                } else {
+                    unchanged++; 
                 }
             }
             
-            // If no changes occurred during this iteration, break the loop
-            if (!changed) {
+            count++; 
+            
+            if (unchanged == arr.length) {
                 break;
             }
-            
-            count++;  // Increment the count for each iteration where changes occurred
         }
         
-        return count;  // Return the number of iterations
+        return count - 1; 
     }
 }
