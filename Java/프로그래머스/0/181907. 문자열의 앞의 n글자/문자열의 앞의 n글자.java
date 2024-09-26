98315d0ce7d948fc3c7b6534acbef81c3faad75d
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-26 10:15:40 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
+++ Java/프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -1,7 +1,5 @@
 public class Solution {
-    public String solution (String my_string, int n) {
-        
-        return my_string.substring(0, n);
-        
+    public String solution (String str, int n) {
+        return str.substring(0, n);
     }
 }
