639a1dc6d83ccb22c470ff7a8744ae3d366d2f80
Jaehyun Kevin Lee <angle0918@naver.com>
2024-09-26 09:22:25 +0900

    [level 0] Title: 배열 만들기 4, Time: 21.62 ms, Memory: 81.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ Java/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -7,27 +7,19 @@ public class Solution {
         
         for (int num : arr) {
             
-            if (stk.isEmpty()) {
-                stk.push(num);
-                continue;
-            }
-            
             while (!stk.isEmpty() && stk.peek() >= num) {
                 stk.pop();
             }
             
             stk.push(num);
-
         }
         
-        int size = stk.size();
+        int[] result = new int[stk.size()];
         
-        int[] result = new int[size];
-        
-        for (int i = size-1; i >= 0; i--) {
+        for (int i = stk.size()-1; i >= 0; i--) {
             result[i] = stk.pop();
         }
-            
+        
         return result;
     }
 }
