6f7b7159d7689d4c8f9955cbe71f802e9720e31d
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-01 09:47:07 +0900

    [level 0] Title: 리스트 자르기, Time: 3.01 ms, Memory: 75.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ Java/프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -3,16 +3,19 @@ import java.util.*;
 public class Solution {
     public int[] solution (int n, int[] slicer, int[] num_list) {
         
-        int s = n == 1 ? 0 : slicer[0];
-        int e = n == 2 ? num_list.length - 1 : slicer[1];
-        int k = n == 4 ? slicer[2] : 1;
+        ArrayList<Integer> result = new ArrayList<>();
         
-        int[] result = new int[(e - s + k)/k];
+        int s = (n == 1) ? 0 : slicer[0];
+        int e = (n == 2) ? num_list.length-1 : slicer[1];
+        int k = (n == 4) ? slicer[2] : 1;
         
-        for (int i = s, j = 0; i <= e; i += k) {
-            result[j++] = num_list[i];
+        int idx = s;
+        
+        while (idx <= e) {
+            result.add(num_list[idx]);
+            idx += k;
         }
         
-        return result;
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
