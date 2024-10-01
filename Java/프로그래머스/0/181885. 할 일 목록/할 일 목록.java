8667ff1c7f8e02d375b19a691a1f24aeb8b71488
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-01 10:18:56 +0900

    [level 0] Title: 할 일 목록, Time: 0.07 ms, Memory: 75.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ Java/프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,39 +1,16 @@
-// public class Solution {
-//     public String[] solution (String[] list, boolean[] fin) {
-        
-//         int len = 0;
-//         for (int j = 0; j < fin.length; j++) {
-//             if (!fin[j]) {
-//                 len++;
-//             }
-//         }
-        
-//         String[] result = new String[len];
-        
-//         for (int i = 0, j = 0; i < list.length; i++) {
-//             if (!fin[i]) {
-//                 result[j++] = list[i];
-//             }
-//         }
-        
-//         return result;
-//     }
-// }
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Solution {
 
-class Solution {
-    public String[] solution(String[] todo_list, boolean[] finished) {
-        List<String> resultList = new ArrayList<>();
+    public String[] solution(String[] todo, boolean[] finished) {
+        List<String> resultList = new ArrayList<>(); 
         
-        for(int i = 0; i < finished.length; i++) {
-            if(!finished[i]) {
-                resultList.add(todo_list[i]);
+        for (int i = 0; i < finished.length; i++) {
+            if (!finished[i]) {
+                resultList.add(todo[i]); 
             }
         }
         
-        // Convert the list to an array
         return resultList.toArray(new String[0]);
     }
 }
