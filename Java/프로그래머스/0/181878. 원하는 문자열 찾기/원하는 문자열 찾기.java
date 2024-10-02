39e637bd5d4b3f7f2b80be16b8878d5b363096a5
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-02 19:49:36 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 10.39 ms, Memory: 78.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ Java/프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
-class Solution {
-    public int solution(String myString, String pat) {
-        myString = myString.toLowerCase();
-        pat = pat.toLowerCase();    
+public class Solution {
+    public int solution (String str, String pat) {
         
-        // Check if myString contains the pattern
-        return (myString.contains(pat)) ? 1 : 0;
+        str = str.toLowerCase();
+        pat = pat.toLowerCase();
+        
+        return str.contains(pat) ? 1 : 0;
+        
     }
 }
