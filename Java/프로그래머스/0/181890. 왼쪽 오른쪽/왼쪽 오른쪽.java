2a320c089f4af670de07264c9bf6f5823ef872ef
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-02 12:33:18 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 66.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ Java/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,19 +1,16 @@
 import java.util.*;
 
 public class Solution {
-    public String[] solution (String[] str_list) {
+    public String[] solution(String[] str_list) {
+        int len = str_list.length;
         
-        for (int i = 0; i < str_list.length; i++) {
+        for (int i = 0; i < len; i++) {
             if (str_list[i].equals("l")) {
-                
                 return Arrays.copyOfRange(str_list, 0, i);
-            
             } else if (str_list[i].equals("r")) {
-                
-                return Arrays.copyOfRange(str_list, i+1, str_list.length);
+                return Arrays.copyOfRange(str_list, i+1, len);
             }
         }
-        
-        return new String[]{};
+        return new String[] {};
     }
 }
