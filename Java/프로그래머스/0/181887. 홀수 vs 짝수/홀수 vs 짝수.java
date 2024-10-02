ee99118e7120303557e0bab2f839934342a309a1
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-02 13:01:49 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.07 ms, Memory: 79.5 MB -BaekjoonHub

--- Java/프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
+++ Java/프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -1,14 +1,17 @@
 public class Solution {
-    public int solution (int[] nums) {
-        
+    public int solution (int[] num_list) {
         int oddSum = 0;
         int evenSum = 0;
         
-        for (int i = 0; i < nums.length; i++) {
-            if (i % 2 == 0) oddSum += nums[i];
-            if (i % 2 == 1) evenSum += nums[i];
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) {
+                oddSum += num_list[i];
+            } else {
+                evenSum += num_list[i];
+            }
         }
         
-        return Math.max(oddSum, evenSum);
+        return Math.max(evenSum, oddSum);
+        
     }
 }
