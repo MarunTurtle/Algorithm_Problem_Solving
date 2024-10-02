6ec5db7e8cdfa8983e1979a39d1e2462be36de39
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-02 12:46:44 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ Java/프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution (int[] nums, int n) {
+    public int[] solution(int[] num_list, int n) {
         
-        int[] result = new int[(nums.length - 1 + n)/n];
+        int len = num_list.length;
+        int size = (len - 1 + n) / n;
+        int[] result = new int[size];
         
-        for (int i = 0, j = 0; i < nums.length; i += n) {
-            result[j++] = nums[i];
+        for (int i = 0, j = 0; i < len; i += n) {
+            result[j++] = num_list[i];
         }
         
         return result;
