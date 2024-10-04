d66e566b89ad7a50c7ceca01b9355eb7abeb85a2
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-04 10:49:03 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.45 ms, Memory: 86.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ Java/프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,26 +1,17 @@
 class Solution {
     public int solution(String myString, String pat) {
-        
-        // StringBuilder to build the modified string with swapped characters
-        StringBuilder newString = new StringBuilder();
-        
-        // Iterate through each character in myString
-        for (int i = 0; i < myString.length(); i++) {
-            // Swap 'B' with 'A'
-            if (myString.charAt(i) == 'B') {
-                newString.append('A');
+        String[] changed = myString.split("");
+
+        for (int i = 0; i < changed.length; i++) {
+            if (changed[i].equals("A")) {
+                changed[i] = "B";
+            } else {
+                changed[i] = "A";
             }
-            // Swap 'A' with 'B'
-            else if (myString.charAt(i) == 'A') {
-                newString.append('B');
-            }
-        } 
-        
-        // Check if the modified string contains the pattern pat
-        if (newString.toString().contains(pat)) {
-            return 1;  // Return 1 if pat is found
-        } else {
-            return 0;  // Return 0 if pat is not found
         }
+
+        String chStr = String.join("", changed);
+
+        return chStr.contains(pat) ? 1 : 0;  
     }
 }
