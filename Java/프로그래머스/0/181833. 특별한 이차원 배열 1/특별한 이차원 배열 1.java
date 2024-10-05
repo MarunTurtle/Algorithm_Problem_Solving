924a9b038096ba5f9bd621a2b2bd85abb40eb375
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-05 14:25:33 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.42 ms, Memory: 87.5 MB -BaekjoonHub

--- Java/프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ Java/프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -1,6 +1,16 @@
-class Solution {
-    public int[][] solution(int n) {
-        int[][] answer = {};
-        return answer;
+import java.util.*;
+
+public class Solution {
+    public int[][] solution (int n) {
+        
+        int[][] result = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = (i == j) ? 1 : 0;
+            }
+        }
+        
+        return result;
     }
 }
