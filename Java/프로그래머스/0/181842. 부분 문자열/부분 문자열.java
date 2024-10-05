66f377c06bc9abd201670c6eecad0165452d6976
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-05 11:31:49 +0900

    [level 0] Title: 부분 문자열, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ Java/프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,7 +1,5 @@
-class Solution {
-    public int solution(String str1, String str2) {
-        
-        return str2.contains(str1) ? 1 : 0;       
-        
+public class Solution {
+    public int solution (String str1, String str2) {
+        return str2.contains(str1) ? 1 : 0;
     }
 }
