5b9266cf4bc3b2cbd9d9f1045c248953b8764eb6
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-05 11:23:00 +0900

    [level 0] Title: 정수 부분, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181850. 정수 부분/정수 부분.java
+++ Java/프로그래머스/0/181850. 정수 부분/정수 부분.java
@@ -1,5 +1,7 @@
-class Solution {
-    public int solution(double flo) {
-        return (int) Math.floor(flo);
+import java.util.*;
+
+public class Solution {
+    public int solution (double flo) {
+        return (int) flo;
     }
 }
