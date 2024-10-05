ec3626da4ab6e93fd36a2c845772e265e32b7ba3
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-05 15:01:31 +0900

    [level 0] Title: 배열 조각하기, Time: 0.04 ms, Memory: 75.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ Java/프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -2,29 +2,18 @@ import java.util.*;
 
 public class Solution {
     
-    static ArrayList<Integer> result;
     static int s;
     static int e;
     
     public int[] solution (int[] arr, int[] query) {
-        
-        result = new ArrayList<>();
-        s = 0;
-        e = 0;
-        
         for (int i = 0; i < query.length; i++) {
-            if (i % 2 == 0) { 
+            if (i % 2 == 0) {
                 e = s + query[i];
             } else {
-                s = s + query[i];               
-            }          
-        }
+                s = s + query[i];
+            }
+        }   
         
         return Arrays.copyOfRange(arr, s, e+1);
     }
-        
-        
-        
-    
-        
 }
