1714cb0553fe8fbd64803bfa5fa31b5a8ac74ec1
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-07 19:26:01 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.04 ms, Memory: 74.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ Java/프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,25 +1,15 @@
-// import java.util.*;
-
-// public class Solution {
-//     public int[] solution (int[] num_list, int n) {
-        
-//         int[] result = new int[num_list.length];
-        
-//         System.arraycopy(num_list, n, result, 0, num_list.length - n); 
-//         System.arraycopy(num_list, 0, result, num_list.length - n, n);
-//         return result;
-//     }
-// }
-
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int[] nums, int n) {
+    public int[] solution (int[] num, int n) {
         
-        int[] result = new int[nums.length];
+        int len = num.length;
         
-        System.arraycopy(nums, n, result, 0, nums.length-n);
-        System.arraycopy(nums, 0, result, nums.length-n, n);
+        int[] result = new int[len];
+        
+        System.arraycopy(num, n, result, 0, len - n);
+        
+        System.arraycopy(num, 0, result, len - n, n);
         
         return result;
         
