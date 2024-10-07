78628920de23122662b01383c042fd70625f13b5
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-07 21:29:07 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.69 ms, Memory: 75.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ Java/프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,39 +1,15 @@
-import java.util.ArrayList;
+import java.util.*;
 
-class Solution {
-    public String[] solution(String my_string) {
+public class Solution {
+    public String[] solution(String myStr) {
+        StringTokenizer st = new StringTokenizer(myStr);
+        int N = st.countTokens();
+        String[] result = new String[N];
         
-        // Declare ArrayList to store the result
-        ArrayList<String> result = new ArrayList<>();
-        
-        // Declare length of the string
-        int length = my_string.length();
-        
-        // Temporary StringBuilder to accumulate characters of a word
-        StringBuilder word = new StringBuilder();
-        
-        // Iterate through the string character by character
-        for (int i = 0; i < length; i++) {
-            char currentChar = my_string.charAt(i);
-            
-            // If the current character is not a space, add it to the current word
-            if (currentChar != ' ') {
-                word.append(currentChar);
-            } else {
-                // If the current character is a space and we have a word, add it to the list
-                if (word.length() > 0) {
-                    result.add(word.toString());
-                    word.setLength(0); // Clear the StringBuilder for the next word
-                }
-            }
+        for (int i = 0; i < N; i++) {
+            result[i] = st.nextToken();
         }
         
-        // Add the last word to the list if there is one
-        if (word.length() > 0) {
-            result.add(word.toString());
-        }
-        
-        // Convert ArrayList to String array and return
-        return result.toArray(new String[0]);
+        return result;
     }
 }
