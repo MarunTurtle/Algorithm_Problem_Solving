8dfc4a564e1d7f12560ce7f1b77d8ddc8487f51f
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-07 21:38:14 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.35 ms, Memory: 71.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ Java/프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,16 +1,18 @@
 public class Solution {
     public int solution (String myStr, String pat) {
-        String[] strArr = myStr.split("");
         
-        for (int i = 0; i < strArr.length; i ++) {
-            if (strArr[i].equals("B")) {
-                strArr[i] = "A"; 
+        int len = myStr.length();
+        
+        String[] result = myStr.split("");
+        
+        for (int i = 0; i < len; i++) {
+            if (result[i].equals("A")) {
+                result[i] = "B";
             } else {
-                strArr[i] = "B"; 
+                result[i] = "A";
             }
         }
         
-        String result = String.join("", strArr);
-        return result.contains(pat) ? 1 : 0;
-     }
+        return String.join("", result).contains(pat) ? 1 : 0;
+    }
 }
