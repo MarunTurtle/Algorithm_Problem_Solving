a7b6625a8339792b6362269cc68ff84de8edb3a4
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-07 19:40:31 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181886. 5명씩/5명씩.java
+++ Java/프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,13 +1,12 @@
 public class Solution {
-    public String[] solution(String[] names) {
+    public String[] solution (String[] names) {
         
         int len = names.length;
-        String[] result = new String[(len - 1 + 5)/5];
         
-        for(int i = 0, j = 0; i < len; i++) {
-            if (i % 5 == 0) {
-                result[j++] = names[i];
-            }
+        String[] result = new String[(len -1 + 5)/5];
+        
+        for (int i = 0, j = 0; i < len; i += 5) {
+            result[j++] = names[i]; 
         }
         
         return result;
