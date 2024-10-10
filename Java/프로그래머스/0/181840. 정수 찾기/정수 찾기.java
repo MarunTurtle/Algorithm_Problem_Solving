d7fa5e3a2491660414610475ec766c0629bafcdd
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-10 20:15:41 +0900

    [level 0] Title: 정수 찾기, Time: 0.54 ms, Memory: 75.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ Java/프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,26 +1,25 @@
 import java.util.*;
 
 public class Solution {
-    public int solution(int[] numList, int n) {
-        // Sort the array to prepare for binary search
+    public int solution (int[] numList, int n) {
+        
         Arrays.sort(numList);
         
-        int left = 0;
-        int right = numList.length - 1;
-
-        // Binary search
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            
-            if (numList[mid] == n) {
-                return 1;  // Target found
-            } else if (numList[mid] < n) {
-                left = mid + 1;  // Search the right half
-            } else {
-                right = mid - 1;  // Search the left half
-            }
-        }
+        int len = numList.length;
+        int s = 0;
+        int e = len-1;
+        int mid; 
         
-        return 0;  // Target not found
+        while (s <= e) {
+            mid = (s + e) / 2;
+            if (numList[mid] == n) {
+                return 1;
+            } else if (numList[mid] > n) {
+                e = mid-1;
+            } else {
+                s = mid+1;
+            }   
+        }
+        return 0;
     }
 }
