85f26fb5974cac563ddbe466d9820bdca02580c9
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-16 12:16:33 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.04 ms, Memory: 82 MB -BaekjoonHub

--- Java/프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ Java/프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,12 +1,11 @@
-class Solution {
-    public int solution(int[] date1, int[] date2) {
+public class Solution {
+    public int solution (int[] date1, int[] date2) {
+        
         for (int i = 0; i < 3; i++) {
-            if (date1[i] < date2[i]) {
-                return 1;  // date1이 date2보다 이전인 경우
-            } else if (date1[i] > date2[i]) {
-                return 0;  // date1이 date2보다 이후인 경우
-            }
+            if (date1[i] < date2[i]) return 1;
+            else if (date1[i] > date2[i]) return 0;
         }
-        return 0;  // 날짜가 동일한 경우
+        
+        return 0;
     }
 }
