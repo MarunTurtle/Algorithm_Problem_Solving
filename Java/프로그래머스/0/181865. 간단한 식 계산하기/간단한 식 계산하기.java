b7e73c2d954a1acdc440ed2ba1174cf1b5227683
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-18 09:44:51 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.18 ms, Memory: 66.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ Java/프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -1,26 +1,15 @@
-class Solution {
+public class Solution {
     public int solution(String binomial) {
-        // "a op b" 형태의 이항식을 공백으로 분리
-        String[] biArr = binomial.trim().split(" ");
+        String[] parsed = binomial.split(" ");
+        int a = Integer.parseInt(parsed[0]);
+        int b = Integer.parseInt(parsed[2]);
         
-        // 첫 번째와 세 번째 요소를 정수로 변환
-        int a = Integer.parseInt(biArr[0]);
-        int b = Integer.parseInt(biArr[2]);
-        
-        // 연산자에 따라 계산 수행
-        switch (biArr[1]) {
-            case "+" -> {
-                return a + b;
-            }
-            case "-" -> {
-                return a - b;
-            }
-            case "*" -> {
-                return a * b;
-            }
-            default -> {
-                throw new IllegalArgumentException("Invalid operator: " + biArr[1]);
-            }
+        if (parsed[1].equals("+")) {
+            return a + b;
+        } else if (parsed[1].equals("-")) {
+            return a - b;
+        } else {
+            return a * b;
         }
     }
 }
