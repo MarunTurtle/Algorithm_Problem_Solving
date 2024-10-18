32fe63cd24d41b7598774ee7745075e0d3cfc088
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-18 14:16:15 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.48 ms, Memory: 76.5 MB -BaekjoonHub

--- Java/프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ Java/프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -1,27 +1,14 @@
-class Solution {
-    public int solution(String num_str) {
-        // 문자열을 한 자리씩 분리하여 배열로 만듦
-        String[] strArr = num_str.split("");
+public class Solution {
+    public int solution (String numStr) {
+        
+        String[] strArr = numStr.split("");
+        
         int sum = 0;
         
-        // 배열의 각 원소를 정수로 변환하여 합계 계산
-        for (String s : strArr) {
-            sum += Integer.parseInt(s);
+        for (String str : strArr) {
+            sum += Integer.parseInt(str);
         }
         
         return sum;
     }
 }
-
-// class Solution {
-//     public int solution(String num_str) {
-//         int sum = 0;
-        
-//         // 문자열의 각 문자를 숫자로 변환하여 더하기
-//         for (int i = 0; i < num_str.length(); i++) {
-//             sum += Character.getNumericValue(num_str.charAt(i));
-//         }
-        
-//         return sum;
-//     }
-// }
