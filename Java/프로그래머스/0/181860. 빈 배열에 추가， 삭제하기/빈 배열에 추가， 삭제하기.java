eec5d327640a0aa53408b9fd5b01a8032895976f
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-18 12:15:54 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 3.49 ms, Memory: 77.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
+++ Java/프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -1,29 +1,25 @@
-import java.util.ArrayList;
+import java.util.*;
 
-class Solution {
+public class Solution {
     public int[] solution(int[] arr, boolean[] flag) {
-        ArrayList<Integer> X = new ArrayList<>();
         
-        for (int i = 0; i < arr.length; i++) {
+        int len = flag.length;
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < len; i++) {
             if (flag[i]) {
-                // Add arr[i] to X arr[i] * 2 times
-                for (int j = 0; j < arr[i] * 2; j++) {
-                    X.add(arr[i]);
+                int j = 0;
+                while (j < arr[i] * 2) {
+                    result.add(arr[i]);
+                    j++;
                 }
             } else {
-                // Remove the last arr[i] elements from X
                 for (int j = 0; j < arr[i]; j++) {
-                    X.remove(X.size() - 1);
+                    result.remove(result.size() - 1);
                 }
             }
         }
-        
-        // Convert the ArrayList to an int array
-        int[] result = new int[X.size()];
-        for (int i = 0; i < X.size(); i++) {
-            result[i] = X.get(i);
-        }
-        
-        return result;
+     
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
