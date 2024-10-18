ca35bf650e34e61cf7638e367cc79e53c8699299
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-18 11:54:59 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 20.89 ms, Memory: 76.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
+++ Java/프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
@@ -1,23 +1,18 @@
-class Solution {
-    public int[] solution(int[] arr) {
+import java.util.*;
+
+public class Solution {
+    public int[] solution (int[] arr) {
+    
+        List<Integer> result = new ArrayList<>();
+        int len = arr.length;
         
-        // Calculate the size of the result array
-        int size = 0;
-        for (int i = 0; i < arr.length; i++) {
-            size += arr[i];
-        }
-        
-        // Declare the result array with the calculated size
-        int[] result = new int[size];
-        
-        // Fill the result array
-        int index = 0;  // Current position in the result array
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < len; i++) {
             for (int j = 0; j < arr[i]; j++) {
-                result[index++] = arr[i];
+                result.add(arr[i]);
             }
         }
-        
-        return result;
+    
+        // Better to pass an array of the required size
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
