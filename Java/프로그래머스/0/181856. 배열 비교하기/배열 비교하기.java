e7e35b86e1ca66861cf439f3ef9e7d6300d465f6
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-18 12:30:40 +0900

    [level 0] Title: 배열 비교하기, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ Java/프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -4,20 +4,18 @@ public class Solution {
     public int solution (int[] arr1, int[] arr2) {
         int len1 = arr1.length;
         int len2 = arr2.length;
-        int sum1 = 0;
-        int sum2 = 0;
-        
-        for (int i = 0; i < arr1.length; i++) {
-            sum1 += arr1[i];
-        }
-        
-        for (int j = 0; j < arr2.length; j++) {
-            sum2 += arr2[j];
-        }
                 
         int result = compare(len1, len2);
         
         if (result == 0) {
+            int sum1 = 0;
+            int sum2 = 0;
+            
+            for (int i = 0; i < len1; i++) {
+                sum1 += arr1[i];
+                sum2 += arr2[i];
+            }
+            
             int result2 = compare(sum1, sum2);
             return result2;
         } else {
