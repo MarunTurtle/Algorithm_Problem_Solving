471897e1068a8113869137d72740548e894927ab
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-18 12:25:26 +0900

    [level 0] Title: 배열 비교하기, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

--- Java/프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ Java/프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,23 +1,35 @@
 import java.util.*;
 
-class Solution {
-    public int solution(int[] arr1, int[] arr2) {
-        // Compare lengths of the arrays
-        int lengthComparison = Integer.compare(arr1.length, arr2.length);
+public class Solution {
+    public int solution (int[] arr1, int[] arr2) {
+        int len1 = arr1.length;
+        int len2 = arr2.length;
+        int sum1 = 0;
+        int sum2 = 0;
         
-        if (lengthComparison != 0) {
-            return lengthComparison;
-        } else {
-            // Lengths are equal, so compare the sums of the arrays
-            int sum1 = 0;
-            int sum2 = 0;
-            for (int num : arr1) {
-                sum1 += num;
-            }
-            for (int num : arr2) {
-                sum2 += num;
-            }
-            return Integer.compare(sum1, sum2);
+        for (int i = 0; i < arr1.length; i++) {
+            sum1 += arr1[i];
         }
+        
+        for (int j = 0; j < arr2.length; j++) {
+            sum2 += arr2[j];
+        }
+                
+        int result = compare(len1, len2);
+        
+        if (result == 0) {
+            int result2 = compare(sum1, sum2);
+            return result2;
+        } else {
+            return result;
+        }
+                
+        
+    }
+    
+    public static int compare (int a, int b) {
+        if (a > b) return 1;
+        else if (a < b) return -1;
+        else return 0;
     }
 }
