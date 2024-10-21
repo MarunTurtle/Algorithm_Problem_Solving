4ebcaa38c2f21aeb7e408e7041fbba4379958e4d
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-21 09:23:39 +0900

    [level 0] Title: 배열 만들기 6, Time: 33.23 ms, Memory: 136 MB -BaekjoonHub

--- Java/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ Java/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -1,35 +1,28 @@
 import java.util.*;
 
-class Solution {
-    public int[] solution(int[] arr) {
+public class Solution {
+    public int[] solution (int[] arr) {
         
-        // Declare stk as a Deque (double-ended queue)
-        Deque<Integer> stk = new ArrayDeque<>();        
+        Deque<Integer> stk = new ArrayDeque<>();
         
-        // Iterate through elements in arr
-        for (int i = 0; i < arr.length; i++) {
-            // If the stack is empty, push arr[i]
+        int len = arr.length;
+        
+        for (int i = 0; i < len; i++) {
             if (stk.isEmpty()) {
                 stk.push(arr[i]);
-            // If the top of the stack is the same as arr[i], pop the stack
             } else if (stk.peek() == arr[i]) {
                 stk.pop();
-            // If the top of the stack is different, push arr[i]
             } else {
                 stk.push(arr[i]);
             }
         }
         
-        // If the stack is empty after processing, add -1
-        if (stk.isEmpty()) {
-            stk.push(-1);
-        }
+        if (stk.isEmpty()) return new int[] {-1};
         
-        // Convert the stack to an array to return
         int[] result = new int[stk.size()];
-        int i = stk.size() - 1;
+        int j = stk.size()-1;
         while (!stk.isEmpty()) {
-            result[i--] = stk.pop();
+            result[j--] = stk.pop();
         }
         
         return result;
