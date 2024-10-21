f99d5717ac4410f0edf3d73c29a0125aa29153b7
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-21 10:23:38 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.05 ms, Memory: 80.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ Java/프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,34 +1,34 @@
-class Solution {
+public class Solution {
     public int[][] solution (int n) {
         
-        int t = n * n;
         int[][] matrix = new int[n][n];
         
-        int top = 0, bottom = n-1;
-        int left = 0, right = n-1;
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = n - 1;
         
         int value = 1;
         
-        while (value <= t) {
-            
-            // top
+        while (value <= n*n) {
+             // top
             for (int i = left; i <= right; i++) {
-                matrix[top][i] = value++;
+                matrix[top][i] = value++; 
             }
             top++;
-            
+
             // right
             for (int i = top; i <= bottom; i++) {
                 matrix[i][right] = value++;
             }
             right--;
-            
+
             // bottom
             for (int i = right; i >= left; i--) {
                 matrix[bottom][i] = value++;
             }
             bottom--;
-            
+
             // left
             for (int i = bottom; i >= top; i--) {
                 matrix[i][left] = value++;
