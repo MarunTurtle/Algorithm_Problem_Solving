0d94d8c10eadedf39272097a1bc95c35a22a117d
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-21 10:43:56 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 2.06 ms, Memory: 76.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
+++ Java/프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -1,23 +1,20 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution (int[] arr, int k) {
+    public int[] solution (int arr[], int k) {
         
         int[] result = new int[k];
         Arrays.fill(result, -1);
         
-        HashSet<Integer> seen = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();
         
-        int len = arr.length;
-        
-        for (int i = 0, j = 0; i < len && j < k; i++) {
+        for (int i = 0, j = 0; i < arr.length && j < k; i++) {
             if (!seen.contains(arr[i])) {
-                result[j++] = arr[i];
                 seen.add(arr[i]);
+                result[j++] = arr[i];
             }
         }
-        
-        return result;
             
+        return result;
     }
 }
