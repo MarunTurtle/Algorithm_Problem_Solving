92ccd2d881b96841c231c05812fc8f601fe92fae
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-21 12:10:12 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 2.27 ms, Memory: 80.5 MB -BaekjoonHub

--- Java/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ Java/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 public class Solution {
-    public int solution(int[] rank, boolean[] attendance) {
+    public int solution (int[] rank, boolean[] attendance) {
         
-        HashMap<Integer, Integer> ranks = new HashMap<>();
+        HashMap<Integer, Integer> rankMap = new HashMap<>();
         
         for (int i = 0; i < rank.length; i++) {
-            if (attendance[i]) {
-                ranks.put(i, rank[i]);
-            }
+            if (attendance[i]) rankMap.put(i, rank[i]);
         }
         
-        List<Map.Entry<Integer, Integer>> sortedRanks = new ArrayList<>(ranks.entrySet());
+        List<HashMap.Entry<Integer, Integer>> sortedRanks = new ArrayList<>(rankMap.entrySet());
+        
         sortedRanks.sort(Map.Entry.comparingByValue());
         
         int a = sortedRanks.get(0).getKey();
         int b = sortedRanks.get(1).getKey();
         int c = sortedRanks.get(2).getKey();
-        
+            
         return 10000 * a + 100 * b + c;
+                
     }
 }
