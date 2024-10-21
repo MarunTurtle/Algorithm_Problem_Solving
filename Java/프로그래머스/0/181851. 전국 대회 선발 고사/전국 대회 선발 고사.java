30456c96e51e843453dc638d5bf865b8acce207d
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-21 10:06:03 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 3.31 ms, Memory: 76.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ Java/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,41 +1,23 @@
-// import java.util.*;
+import java.util.*;
 
-// class Solution {
-//     public int solution(int[] rank, boolean[] attendance) {
-//         // Create a list of students who can attend
-//         List<Integer> attendees = new ArrayList<>();
-        
-//         for (int i = 0; i < rank.length; i++) {
-//             if (attendance[i]) {
-//                 attendees.add(i);
-//             }
-//         }
-        
-//         // Sort the attendees based on their ranks
-//         attendees.sort(Comparator.comparingInt(i -> rank[i]));
-        
-//         // Get the top 3 students
-//         int a = attendees.get(0);
-//         int b = attendees.get(1);
-//         int c = attendees.get(2);
-        
-//         // Return the result based on the formula
-//         return 10000 * a + 100 * b + c;
-//     }
-// }
-
-
-import java.util.PriorityQueue;
-
-class Solution {
+public class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-
-        PriorityQueue<Integer> que = new PriorityQueue<>((a, b) -> rank[a] - rank[b]);
-        for (int i = 0; i < attendance.length; i++) {
-            if (attendance[i])
-                que.add(i);
+        
+        HashMap<Integer, Integer> ranks = new HashMap<>();
+        
+        for (int i = 0; i < rank.length; i++) {
+            if (attendance[i]) {
+                ranks.put(i, rank[i]);
+            }
         }
-
-        return que.poll() * 10000 + que.poll() * 100 + que.poll();
+        
+        List<Map.Entry<Integer, Integer>> sortedRanks = new ArrayList<>(ranks.entrySet());
+        sortedRanks.sort(Map.Entry.comparingByValue());
+        
+        int a = sortedRanks.get(0).getKey();
+        int b = sortedRanks.get(1).getKey();
+        int c = sortedRanks.get(2).getKey();
+        
+        return 10000 * a + 100 * b + c;
     }
 }
