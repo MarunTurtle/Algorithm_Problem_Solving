a3bbacfba90643df23a64d85b1ca566882dc2541
Jaehyun Kevin Lee <angle0918@naver.com>
2024-10-21 10:16:26 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 8.24 ms, Memory: 72.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ Java/프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -2,25 +2,18 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int[] arr, int[] delete) {
-        Deque<Integer> arrQue = new ArrayDeque<>();
-        for (int num : arr) {
-            arrQue.offer(num);
-        }
-
-        // 삭제할 값들을 저장하는 Set을 사용 (중복 제거와 빠른 탐색)
         Set<Integer> deleteSet = new HashSet<>();
         for (int num : delete) {
             deleteSet.add(num);
         }
-
-        int originalSize = arrQue.size();
-        for (int i = 0; i < originalSize; i++) {
-            int temp = arrQue.poll();
-            if (!deleteSet.contains(temp)) {
-                arrQue.offer(temp);
+        
+        List<Integer> resultList = new ArrayList<>();
+        for (int num : arr) {
+            if (!deleteSet.contains(num)) {
+                resultList.add(num);
             }
         }
-
-        return arrQue.stream().mapToInt(i -> i).toArray();
+              
+        return resultList.stream().mapToInt(i -> i).toArray();
     }
 }
